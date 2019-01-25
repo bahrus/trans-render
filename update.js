fec49ed55cb184d039217e34b9cbd51475ca18a8
bahrus <anderson.bruce.b@gmail.com>
2019-01-25 11:45:24 -0500

    ...

--- update.js
+++ update.js
@@ -1,11 +1,11 @@
 import { process } from './init.js';
-export function update(ctx, target) {
+export function update(ctx, target, options) {
     const updateCtx = ctx;
     updateCtx.update = update;
     const firstChild = target.firstElementChild;
     if (firstChild !== null) {
         ctx.leaf = firstChild;
-        process(ctx, 0, 0);
+        process(ctx, 0, 0, options);
     }
     return updateCtx;
 }
--- update.ts
+++ update.ts
@@ -1,15 +1,15 @@
-import {RenderContext} from './init.d.js';
+import {RenderContext, RenderOptions} from './init.d.js';
 import {UpdateContext} from './update.d.js';
 import {process} from './init.js';
 
 
-export function update(ctx: RenderContext, target: HTMLElement | DocumentFragment){
+export function update(ctx: RenderContext, target: HTMLElement | DocumentFragment, options?: RenderOptions){
     const updateCtx = ctx as UpdateContext;
     updateCtx.update = update;
     const firstChild = target.firstElementChild;
     if(firstChild !== null){
         ctx.leaf = firstChild;
-        process(ctx, 0, 0);
+        process(ctx, 0, 0, options);
     }
     return updateCtx;
 }
