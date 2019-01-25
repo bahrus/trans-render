1db13f867c992397f21f22ade54821c7c741f83f
bahrus <anderson.bruce.b@gmail.com>
2019-01-25 17:36:15 -0500

    ...

---
+++ x.js
@@ -0,0 +1,11 @@
+import { init } from './init.js';
+import { update } from './update.js';
+import { repeatInit } from './repeatInit.js';
+import { repeatUpdate } from './repeatUpdate.js';
+export const x = {
+    i: init,
+    u: update,
+    rpi: repeatInit,
+    rpu: repeatUpdate
+};
+//# sourceMappingURL=x.js.map
---
+++ x.ts
@@ -0,0 +1,10 @@
+import { init } from './init.js';
+import { update } from './update.js';
+import { repeatInit } from './repeatInit.js';
+import { repeatUpdate } from './repeatUpdate.js';
+export const x = {
+    i: init,
+    u: update,
+    rpi: repeatInit,
+    rpu: repeatUpdate
+}
