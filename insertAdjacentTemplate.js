eccc1a2eec78c72d058ec50adc3cfc8b55004525
bahrus <anderson.bruce.b@gmail.com>
2019-02-12 07:02:46 -0500

    ...

--- insertAdjacentTemplate.js
+++ insertAdjacentTemplate.js
@@ -1,6 +1,20 @@
 export function insertAdjacentTemplate(src, template) {
+    src.style.display = 'none';
+    let targetToAppend = src;
     const clonedTemplate = template.content.cloneNode(true);
     Array.from(clonedTemplate.children).forEach(child => {
-        src.insertAdjacentElement('afterend', child);
+        let slot = null;
+        if (child.localName === 'slot') {
+            slot = child;
+        }
+        else {
+            slot = child.querySelector('slot');
+        }
+        if (slot !== null) {
+            while (src.lastElementChild) {
+                slot.insertAdjacentElement('afterend', src.lastElementChild);
+            }
+        }
+        targetToAppend = targetToAppend.insertAdjacentElement('afterend', child);
     });
 }
--- insertAdjacentTemplate.ts
+++ insertAdjacentTemplate.ts
@@ -1,6 +1,19 @@
 export function insertAdjacentTemplate(src: HTMLElement, template: HTMLTemplateElement){
+    src.style.display = 'none';
+    let targetToAppend = src;
     const clonedTemplate = template.content.cloneNode(true) as DocumentFragment;
     Array.from(clonedTemplate.children).forEach(child =>{
-        src.insertAdjacentElement('afterend', child);
+        let slot: HTMLSlotElement | null = null;
+        if(child.localName === 'slot'){
+            slot = child as HTMLSlotElement;
+        }else{
+            slot = child.querySelector('slot');
+        }
+        if(slot !== null){
+            while (src.lastElementChild) {
+                slot.insertAdjacentElement('afterend', src.lastElementChild);
+            }
+        }
+        targetToAppend = targetToAppend.insertAdjacentElement('afterend', child)! as HTMLElement;
     })
 }
