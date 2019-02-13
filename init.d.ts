b21d16e5de6ca3df2efdc4b0aaccb9588b63eab6
bahrus <anderson.bruce.b@gmail.com>
2019-02-13 04:35:09 -0500

    ...

--- init.d.ts
+++ init.d.ts
@@ -1,5 +1,5 @@
 export type TransformFn = (arg: TransformArg) => TransformRules | NextStep | string | void;
-export type TransformValueOptions =  TransformRules | TransformFn;
+export type TransformValueOptions =  TransformRules | TransformFn | string;
 export type TransformRules = { [key: string]: TransformValueOptions};
 export interface TransformArg {
     target: Element,
