90d8b6af9ffa7bc767c1fbf3edb86260201ed2c9
bahrus <anderson.bruce.b@gmail.com>
2019-07-10 18:31:55 -0400

    ...

--- replaceTargetWithTag.js
+++ replaceTargetWithTag.js
@@ -4,7 +4,7 @@ import { deleteMe } from './init.js';
  * @param target
  * @param template
  */
-export function replaceTargetWithTemplate(target, tag, ctx) {
+export function replaceTargetWithTag(target, tag, ctx) {
     const tagEl = document.createElement(tag);
     target.insertAdjacentElement('afterend', tagEl);
     ctx.replacedElement = target;
--- replaceTargetWithTag.ts
+++ replaceTargetWithTag.ts
@@ -6,7 +6,7 @@ import {RenderContext} from './init.d.js';
  * @param target 
  * @param template 
  */
-export function replaceTargetWithTemplate(target: Element, tag: string, ctx: RenderContext){
+export function replaceTargetWithTag(target: Element, tag: string, ctx: RenderContext){
     const tagEl = document.createElement(tag);
     target.insertAdjacentElement('afterend', tagEl);
     ctx.replacedElement = target;
