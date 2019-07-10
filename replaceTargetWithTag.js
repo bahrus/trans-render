6beb940cc97b3ed4f965e04ae9842acd781cbed2
bahrus <anderson.bruce.b@gmail.com>
2019-07-10 19:45:47 -0400

    ...

--- replaceTargetWithTag.js
+++ replaceTargetWithTag.js
@@ -4,9 +4,11 @@ import { deleteMe } from './init.js';
  * @param target
  * @param template
  */
-export function replaceTargetWithTag(target, tag, ctx) {
+export function replaceTargetWithTag(target, tag, ctx, postSwapCallback) {
     const tagEl = document.createElement(tag);
     target.insertAdjacentElement('afterend', tagEl);
     ctx.replacedElement = target;
     target[deleteMe] = true;
+    if (postSwapCallback)
+        postSwapCallback(target, tagEl);
 }
--- replaceTargetWithTag.ts
+++ replaceTargetWithTag.ts
@@ -6,9 +6,10 @@ import {RenderContext} from './init.d.js';
  * @param target 
  * @param template 
  */
-export function replaceTargetWithTag(target: Element, tag: string, ctx: RenderContext){
+export function replaceTargetWithTag(target: Element, tag: string, ctx: RenderContext, postSwapCallback?: (src: Element, dest: HTMLElement) => void){
     const tagEl = document.createElement(tag);
     target.insertAdjacentElement('afterend', tagEl);
     ctx.replacedElement = target;
     (<any>target)[deleteMe] = true;
+    if(postSwapCallback) postSwapCallback(target, tagEl);
 }
