038313575d46adf4b2a07f0f058165b763d4344a
Bruce Anderson <anderson.bruce.b@gmail.com>
2019-11-16 19:18:55 -0500

    ...

---
+++ waitForAttributeChange.ts
@@ -0,0 +1,29 @@
+export function waitForAttributeChange(el: HTMLElement, attributeName: string, once: boolean = true, test?: (s: string | null) => boolean){
+    return new Promise((resolve, reject) =>{
+        const observer = new MutationObserver(mutations => {
+            // For the sake of...observation...let's output the mutation to console to see how this all works
+            mutations.forEach(mutation => {
+                if(mutation.attributeName === attributeName){
+                    if(test){
+                        if(test(el.getAttribute(attributeName))){
+                            resolve();
+                            if(once){
+                                observer.disconnect();
+                            }
+                        }
+                    }else{
+                        resolve();
+                    }
+                }
+            });    
+        });
+         
+        // Notify me of everything!
+        const observerConfig = {
+            attributes: true, 
+        };
+         
+        observer.observe(el, observerConfig);
+    });
+
+}
