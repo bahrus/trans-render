f5b3b9f8d455ccc791e0cac6766a9e089cd61db4
Bruce Anderson <anderson.bruce.b@gmail.com>
2019-11-16 19:50:58 -0500

    ...

--- waitForAttributeChange.ts
+++ waitForAttributeChange.ts
@@ -1,4 +1,6 @@
 export function waitForAttributeChange(el: HTMLElement, attributeName: string, once: boolean = true, test?: (s: string | null) => boolean){
+    //does it even make sense to listen more than once with a promise?
+    //observer of some sort?
     return new Promise((resolve, reject) =>{
         const observer = new MutationObserver(mutations => {
             // For the sake of...observation...let's output the mutation to console to see how this all works
