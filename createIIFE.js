e04aa3adb357a1d33ae5b468e12f723d83f8d2a4
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-03-17 07:18:27 -0400

    ...

--- createIIFE.js
+++
@@ -1,4 +0,0 @@
-const jiife = require('jiife');
-jiife.processFiles(['init.js'], 'dist/init.js', true);
-jiife.processFiles(['update.js'], 'dist/update.js', true);
-jiife.processFiles(['interpolate.js'], 'dist/interpolate.js', true);
