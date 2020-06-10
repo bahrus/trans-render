d0d11ac2316e7cc05689afa4c51d02d45aaacd55
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-06-09 22:13:38 -0400

    ...

--- hydrate.js
+++ hydrate.js
@@ -1,3 +1,16 @@
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to get private field on non-instance");
+    }
+    return privateMap.get(receiver);
+};
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to set private field on non-instance");
+    }
+    privateMap.set(receiver, value);
+    return value;
+};
 //export const propUp: unique symbol = Symbol.for('8646ccd5-3ffd-447a-a4df-0022ca3a8155');
 //export const attribQueue: unique symbol = Symbol.for('02ca2c80-68e0-488f-b4b4-6859284848fb');
 /**
@@ -5,56 +18,63 @@
  * @param superClass
  */
 export function hydrate(superClass) {
-    return class extends superClass {
-        constructor() {
-            super(...arguments);
-            this.#conn = false;
-        }
-        #attribQueue;
-        #conn;
-        /**
-         * Set attribute value.
-         * @param name
-         * @param val
-         * @param trueVal String to set attribute if true.
-         */
-        attr(name, val, trueVal) {
-            if (val === undefined)
-                return this.getAttribute(name);
-            if (!this.#conn) {
-                if (this.#attribQueue === undefined)
-                    this.#attribQueue = [];
-                this.#attribQueue.push({
-                    name, val, trueVal
-                });
-                return;
+    var _attribQueue, _conn, _a;
+    return _a = class extends superClass {
+            constructor() {
+                super(...arguments);
+                _attribQueue.set(this, void 0);
+                _conn.set(this, false);
             }
-            const v = val ? 'set' : 'remove'; //verb
-            this[v + 'Attribute'](name, trueVal || val);
-        }
-        /**
-         * Needed for asynchronous loading
-         * @param props Array of property names to "upgrade", without losing value set while element was Unknown
-         */
-        __propUp(props) {
-            props.forEach(prop => {
-                if (this.hasOwnProperty(prop)) {
-                    let value = this[prop];
-                    delete this[prop];
-                    this[prop] = value;
+            /**
+             * Set attribute value.
+             * @param name
+             * @param val
+             * @param trueVal String to set attribute if true.
+             */
+            attr(name, val, trueVal) {
+                if (val === undefined)
+                    return this.getAttribute(name);
+                if (!__classPrivateFieldGet(this, _conn)) {
+                    if (__classPrivateFieldGet(this, _attribQueue) === undefined)
+                        __classPrivateFieldSet(this, _attribQueue, []);
+                    __classPrivateFieldGet(this, _attribQueue).push({
+                        name, val, trueVal
+                    });
+                    return;
                 }
-            });
-        }
-        connectedCallback() {
-            this.#conn = true;
-            const ep = this.constructor.props;
-            this.__propUp([...ep.bool, ...ep.str, ...ep.num, ...ep.obj]);
-            if (this.#attribQueue !== undefined) {
-                this.#attribQueue.forEach(attribQItem => {
-                    this.attr(attribQItem.name, attribQItem.val, attribQItem.trueVal);
-                });
-                this.#attribQueue = undefined;
+                const v = val ? 'set' : 'remove'; //verb
+                this[v + 'Attribute'](name, trueVal || val);
             }
-        }
-    };
+            /**
+             * Needed for asynchronous loading
+             * @param props Array of property names to "upgrade", without losing value set while element was Unknown
+             */
+            __propUp(props) {
+                const defaultValues = this.constructor['defaultValues'];
+                props.forEach(prop => {
+                    if (this.hasOwnProperty(prop)) {
+                        let value = this[prop];
+                        if (value === undefined && defaultValues !== undefined) {
+                            value = defaultValues[prop];
+                        }
+                        delete this[prop];
+                        this[prop] = value;
+                    }
+                });
+            }
+            connectedCallback() {
+                __classPrivateFieldSet(this, _conn, true);
+                const ep = this.constructor.props;
+                this.__propUp([...ep.bool, ...ep.str, ...ep.num, ...ep.obj]);
+                if (__classPrivateFieldGet(this, _attribQueue) !== undefined) {
+                    __classPrivateFieldGet(this, _attribQueue).forEach(attribQItem => {
+                        this.attr(attribQItem.name, attribQItem.val, attribQItem.trueVal);
+                    });
+                    __classPrivateFieldSet(this, _attribQueue, undefined);
+                }
+            }
+        },
+        _attribQueue = new WeakMap(),
+        _conn = new WeakMap(),
+        _a;
 }
