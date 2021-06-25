312d1136ad841d11b1054e014691f3eb3e548f88
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-06-25 19:50:39 -0400

    ...

--- lib/applyP.ts
+++ lib/applyP.ts
@@ -3,10 +3,12 @@ export function applyP<T extends Partial<HTMLElement> = HTMLElement>(target: T, 
     const props = p[0];
     if (props !== undefined) {
         const safeProps = Object.assign({}, props) as any;
-        delete safeProps.dataset;
-        delete safeProps.style;
-        delete safeProps.localName;
-        delete safeProps.tagName;
+        const readOnlyProps = ['dataset', 'style', 'localName', 'tagName', 'attributes'];
+        for(const prop of readOnlyProps){
+            const val = safeProps[prop];
+            delete safeProps[prop];
+            safeProps['_' + prop] = val;
+        }
         Object.assign(target, safeProps);
         if (props.style !== undefined) Object.assign(target.style, props.style);
         if (props.dataset !== undefined) Object.assign(target.dataset, props.dataset);
