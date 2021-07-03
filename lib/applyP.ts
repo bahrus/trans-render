af2abfeaf8b17e9d693d56ad218d765a0ca455f6
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-03 08:04:32 -0400

    ...

--- lib/applyP.ts
+++ lib/applyP.ts
@@ -3,30 +3,42 @@ export function applyP<T extends Partial<HTMLElement> = HTMLElement>(target: T, 
     const props = p[0];
     if (props !== undefined) {
         const safeProps = {...props} as any;
-        const readOnlyPrimitives = ['localName', 'tagName'];
-        const readOnlyObjects = ['dataset', 'style'];
+        const renameProps = ['localName', 'tagName'];
         const attributes = props.attributes
         if(attributes !== undefined){
             if(attributes instanceof NamedNodeMap){
                 for(const attrib of attributes){
                     target.setAttribute!(attrib.name, attrib.value);
                 }
-                readOnlyObjects.push('attributes');
+                delete safeProps.attributes;
             }else{
-                safeProps['_attributes'] = attributes;
-                readOnlyPrimitives.push('attributes');
+                renameProps.push('attributes');
             }
         }
-        for(const prop of readOnlyPrimitives){
+        const styles = props.style;
+        if(styles !== undefined){
+            if(styles instanceof CSSStyleDeclaration){
+                Object.assign(target.style, styles);
+                delete safeProps.styles;
+            }else{
+                renameProps.push('styles')
+            }
+        }
+        const dataset = props.dataset;
+        if(dataset !== undefined){
+            if(dataset instanceof DOMStringMap){
+                Object.assign(target.dataset, dataset);
+                delete safeProps.dataset;
+            }else{
+                renameProps.push('dataset');
+            }
+        }
+        for(const prop of renameProps){
             const val = safeProps[prop];
             delete safeProps[prop];
             safeProps['_' + prop] = val;
         }
-        for(const prop of readOnlyObjects){
-            delete safeProps[prop];
-        }       
         Object.assign(target, safeProps);
-        if (props.style !== undefined) Object.assign(target.style, props.style);
-        if (props.dataset !== undefined) Object.assign(target.dataset, props.dataset);
+
     }
 }
