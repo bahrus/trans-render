6c331930318b46f2342d6e09ea13d01b4458be6b
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-08-07 21:48:38 -0400

    ...

--- lib/P.ts
+++ lib/P.ts
@@ -2,21 +2,26 @@ import {PMDo, RenderContext, PSettings} from './types.d.js';
 import {applyP} from './applyP.js';
 export class P implements PMDo{
     do(ctx: RenderContext){
-        const rhs = ctx.rhs!;
-        const modifiedRHS: any = {};
-        for(const key in rhs){
-            let val = evalRHS(key, rhs);
-            if(typeof val === 'function'){
-                modifiedRHS[key] = val(ctx);
-            }else{
-                modifiedRHS[key] = val;
-            }
-        }
+        const modifiedRHS = modifyRHS(ctx);
         applyP(ctx.target!, modifiedRHS as PSettings);
     }
 }
 
-export function evalRHS(key: string, rhs: any){
+export function modifyRHS(ctx: RenderContext){
+    const rhs = ctx.rhs!;
+    const modifiedRHS: any = {};
+    for(const key in rhs){
+        let val = evalRHS(key, rhs);
+        if(typeof val === 'function'){
+            modifiedRHS[key] = val(ctx);
+        }else{
+            modifiedRHS[key] = val;
+        }
+    }
+    return modifiedRHS;
+}
+
+function evalRHS(key: string, rhs: any){
     let val = rhs[key];
     if(typeof val === 'string'){
         if(val.startsWith('${') && val.endsWith('}')){
--- lib/PE.ts
+++ lib/PE.ts
@@ -1,20 +1,11 @@
 import {PMDo, RenderContext, PEUnionSettings} from './types.d.js';
-import {evalRHS} from './P.js';
+import {modifyRHS} from './P.js';
 import {applyPE} from './applyPE.js';
 
 export class PE implements PMDo{
     do(ctx: RenderContext){
         if(ctx.host=== undefined) throw 'Unknown host.';
-        const modifiedRHS: any = {};
-        const rhs = ctx.rhs!;
-        for(const key in rhs){
-            let val = evalRHS(key, rhs);
-            if(typeof val === 'function'){
-                modifiedRHS[key] = val(ctx);
-            }else{
-                modifiedRHS[key] = val;
-            }
-        }
+        const modifiedRHS = modifyRHS(ctx);
         applyPE(ctx.host, ctx.target as HTMLElement, modifiedRHS as PEUnionSettings);
     }
 }
