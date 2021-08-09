883b42d23891fca3a873a6e5f1560e0949b4b445
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-08-09 19:30:00 -0400

    ...

--- lib/PEAEval.ts
+++ lib/PEAEval.ts
@@ -1,14 +1,13 @@
 import {PMDo, RenderContext, PEUnionSettings, PEAUnionSettings} from './types.d.js';
-import {modifyPRHS} from './P.js';
-import {modifyERHS} from './PE.js';
+import {modifyRHS} from './PEval.js';
 import {applyPEA} from './applyPEA.js';
 
 export class PEA implements PMDo{
     do(ctx: RenderContext){
         if(ctx.host=== undefined) throw 'Unknown host.';
-        const modifiedProps = modifyPRHS(ctx, 0);
-        const modifiedEvents = modifyERHS(ctx, 1);
-        const modifiedAttribs = modifyPRHS(ctx, 2);
+        const modifiedProps = modifyRHS(ctx, 0);
+        const modifiedEvents = modifyRHS(ctx, 1);
+        const modifiedAttribs = modifyRHS(ctx, 2);
         applyPEA(ctx.host, ctx.target as HTMLElement, [modifiedProps, modifiedEvents, modifiedAttribs] as PEAUnionSettings);
     }
 }
