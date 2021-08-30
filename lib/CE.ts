91d868f58a7c51afbb4ac820333d800bd5fbc65c
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-08-30 17:13:31 -0400

    ...

--- lib/CE.ts
+++ lib/CE.ts
@@ -174,10 +174,10 @@ export class CE<MCProps = any, MCActions = MCProps, TPropInfo = PropInfo, TActio
                     }
                 }
             }
-            connectedCallback(){
-                if(super.connectedCallback) super.connectedCallback();
+            connectedCallback(myArgs = undefined){ //TODO:  problematic
+                if(super.connectedCallback) super.connectedCallback(super.constructor.ceDef);
                 Object.assign(this.style, style);
-                const args = (<any>this.constructor).ceDef;
+                const args = myArgs || (<any>this.constructor).ceDef;
                 const defaults: any = {...args.config.propDefaults, ...args.complexPropDefaults};
                 propUp(this as any as HTMLElement, Object.keys(propInfos), defaults);
                 this.detachQR();
