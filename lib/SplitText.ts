11d511c98c3817d9109f1dfd8b51c63b817a3657
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-09-11 12:13:26 -0400

    ...

--- lib/SplitText.ts
+++ lib/SplitText.ts
@@ -1,5 +1,5 @@
 import {PMDo, RenderContext} from './types.js';
-
+import {getProp} from './getProp.js';
 
 
 export class SplitText implements PMDo{
@@ -9,7 +9,7 @@ export class SplitText implements PMDo{
         if(host === undefined) throw "No host";
         if(textNodes.length === 1){
             const path = textNodes[0];
-            ctx.target!.textContent = path === '.' ? host : host[path];
+            ctx.target!.textContent = path === '.' ? host : getVal(host, path);
         }else{
             ctx.target!.textContent = interpolate(textNodes, host);
         }
@@ -17,9 +17,24 @@ export class SplitText implements PMDo{
     }
 }
 
+function getVal(host:any, path: string): string{
+    const qSplit = path.split('??');
+    const deflt = qSplit[1];
+    const dSplit = qSplit[0].split('.');
+    let val = getProp(host, dSplit);
+    if(val === undefined && deflt){
+        if(deflt[0] === "'"){
+            return deflt.substr(1, deflt.length - 1);
+        }else{
+            return getVal(host, deflt);
+        }
+    }
+    return val;
+}
+
 export function interpolate(textNodes: string[], host: any){
-    return textNodes.map((val, idx) => {
-        if(idx % 2 === 0) return val;
-        return host[val] as string;
+    return textNodes.map((path, idx) => {
+        if(idx % 2 === 0) return path;
+        return getVal(host, path) as string;
     }).join('');
 }
