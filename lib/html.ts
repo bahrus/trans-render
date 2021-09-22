6e9e7d9f0bd6f16c81f94594bfaa70ead9087255
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-09-22 17:57:28 -0400

    ...

--- lib/html.ts
+++ lib/html.ts
@@ -1,4 +1,13 @@
 export function html(strings: TemplateStringsArray, ...keys: string[]) {
+    const out: string[]  = [];
+    for (let i = 0, ii = strings.length; i < ii; i++) {
+        out.push(strings[i]);
+        // if we have a variables for it, we need to bind it.
+        const ithKey = keys[i];
+        if (ithKey !== undefined) {
+          out.push(ithKey);
+        }
+      }
     const templateEl = document.createElement('template');
     templateEl.innerHTML = strings.join('');
     return templateEl;
