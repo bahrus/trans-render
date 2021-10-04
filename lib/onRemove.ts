973a4ded7729ceb1126f5524455804598737b5cc
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-10-04 16:58:44 -0400

    ...

---
+++ lib/onRemove.ts
@@ -0,0 +1,17 @@
+// https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Mutation_events#Mutation_Observers_alternatives_examples
+//can't we use https://developer.mozilla.org/en-US/docs/Web/API/Node/contains#:~:text=The%20Node.,direct%20children%2C%20and%20so%20on.?
+function onRemove(element: Element, callback: Function) {
+    let observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation =>{
+            mutation.removedNodes.forEach(removed =>{
+                if(element === removed){
+                    callback();
+                    observer.disconnect();
+                }
+            })
+        })
+    });
+    observer.observe(element.parentElement || element.getRootNode(), {
+        childList: true,
+    });
+};
