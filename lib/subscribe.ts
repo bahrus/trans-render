bc8954ce7fbb7f0d9003c3526a5be5c4cf130f19
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-01-20 18:41:26 -0500

    0.0.445

---
+++ lib/subscribe.ts
@@ -0,0 +1,42 @@
+export type Callback = (element: Element, propName: string, newValue: any) => void;
+
+const propSubscribers = new WeakMap<Element, {[key: string]: Callback[]}>();
+
+export async function subscribe(element: Element, propName: string, callback: Callback) {
+    if(element.localName.includes('-')){
+        await customElements.whenDefined(element.localName);
+    }
+    if(!propSubscribers.has(element)){
+        propSubscribers.set(element, {
+        });
+    }
+    const subscribers = propSubscribers.get(element)!;
+    if(subscribers[propName] === undefined){
+        subscribers[propName] = [callback];   
+        let proto = element;
+        let prop: PropertyDescriptor | undefined = Object.getOwnPropertyDescriptor(proto, propName);
+        while(proto && !prop){
+            proto = Object.getPrototypeOf(proto);
+            prop = Object.getOwnPropertyDescriptor(proto, propName);
+        }
+        if(prop === undefined){
+            throw {element, propName, message: "Can't find property."};
+        }
+        const setter = prop.set!.bind(element);
+        const getter = prop.get!.bind(element);
+        Object.defineProperty(element, propName, {
+            get(){
+                return getter();
+            },
+            set(nv){
+                setter(nv);
+                const callbacks = subscribers[propName];
+                if(callbacks){
+                    callbacks.forEach(callback => callback(element, propName, nv));
+                }
+            }
+        });
+    }else{
+        subscribers[propName].push(callback);
+    }
+}
