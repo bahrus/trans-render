34b7d0643fd08a84ad721281c9b076f62998637a
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-02-22 18:02:57 -0500

    0.0.490

--- lib/CE.ts
+++ lib/CE.ts
@@ -277,7 +277,14 @@ export class CE<MCProps = any, MCActions = MCProps, TPropInfo = PropInfo, TActio
             const action = actions[methodName];
             if(action.debug) debugger;
             //https://lsm.ai/posts/7-ways-to-detect-javascript-async-function/#:~:text=There%205%20ways%20to%20detect%20an%20async%20function,name%20property%20of%20the%20AsyncFunction%20is%20%E2%80%9CAsyncFunction%E2%80%9D.%202.
-            const isAsync = (<any>target)[methodName].constructor.name === 'AsyncFunction';
+            const method = (<any>target)[methodName];
+            if(method === undefined){
+                throw {
+                    message: `Method ${methodName} not found on ${target.constructor.name}`,
+                    target,
+                }
+            }
+            const isAsync = .constructor.name === 'AsyncFunction';
             const ret = isAsync ? await (<any>target)[methodName](target) : (<any>target)[methodName](target);
             if(ret === undefined) continue;
             self.postHoc(self, action, target, ret, proxy);
