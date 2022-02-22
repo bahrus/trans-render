8bc897d1560a251bad4da7c56a155b09b3f3bd8e
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-02-22 18:44:54 -0500

    0.0.493

--- lib/getVal.ts
+++ lib/getVal.ts
@@ -1,19 +1,36 @@
-export async function getVal(host:any, path: string): Promise<string>{
-    if(host === undefined) return path;
-    if(path[0] !== '.') return host[path];
-    path = path.substr(1);
-    const qSplit = path.split('??');
-    let deflt = qSplit[1];
-    const dSplit = qSplit[0].trim().split('.');
-    const {getProp} = await import('./getProp.js');
-    let val = getProp(host, dSplit);
-    if(val === undefined && deflt){
-        deflt = deflt.trim();
-        if(deflt[0] === "."){
-            return await getVal(host, deflt);
-        }else{
-            return deflt;
+export async function getVal(host: any, path: string): Promise<any> {
+    if (host === undefined)
+        return path;
+    switch(path[0]){
+        case '.':{
+            path = path.substr(1);
+            const qSplit = path.split('??');
+            let deflt = qSplit[1];
+            const dSplit = qSplit[0].trim().split('.');
+            const { getProp } = await import('./getProp.js');
+            let val = getProp(host, dSplit);
+            if (val === undefined && deflt) {
+                deflt = deflt.trim();
+                if (deflt[0] === ".") {
+                    return await getVal(host, deflt);
+                }
+                else {
+                    return deflt;
+                }
+            }
+            return val;
         }
+        case '?':{
+            path = path.substr(1);
+            const qSplit = path.split(' ');
+            const condition = await getVal(host, qSplit[0]) as boolean;
+            const cSplit = qSplit[1].split(':');
+            const idx = condition ? 0 : 1;
+            return await getVal(host, cSplit[idx].trim());
+        }
+        default:
+            return host[path];
     }
-    return val;
+        
+
 }
