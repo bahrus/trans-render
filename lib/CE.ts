02f7dc77bb48e043bce52c3db10c8872b77599aa
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-02-22 18:03:25 -0500

    0.0.491

--- lib/CE.ts
+++ lib/CE.ts
@@ -284,7 +284,7 @@ export class CE<MCProps = any, MCActions = MCProps, TPropInfo = PropInfo, TActio
                     target,
                 }
             }
-            const isAsync = .constructor.name === 'AsyncFunction';
+            const isAsync = method.constructor.name === 'AsyncFunction';
             const ret = isAsync ? await (<any>target)[methodName](target) : (<any>target)[methodName](target);
             if(ret === undefined) continue;
             self.postHoc(self, action, target, ret, proxy);
