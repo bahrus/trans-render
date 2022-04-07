7baafe845a2cda8ed15f0511d69f05562122c1be
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-04-07 16:25:09 -0400

    0.0.518

--- lib/freeze.ts
+++ lib/freeze.ts
@@ -7,9 +7,10 @@ export function freeze(content: DocumentFragment | Element, beHive: Element){
         const beAttr = 'be-' + ifWantsToBe;
         const converted = Array.from(content.querySelectorAll(`[${isAttr}]`));
         for(const el of converted){
-            const attr = el.getAttribute(isAttr)!;
+            const currVal = decorEl.virtualPropsMap.get(el);
+            //const attr = el.getAttribute(isAttr)!;
             el.removeAttribute(isAttr);
-            el.setAttribute(beAttr, attr);
+            el.setAttribute(beAttr, JSON.stringify(currVal));
         }
     }
 }
