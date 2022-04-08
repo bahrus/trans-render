4a0082dd827644eea5bc5c3edae24b837b129d81
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-04-08 04:04:57 -0400

    0.0.521

--- lib/freeze.ts
+++ lib/freeze.ts
@@ -1,5 +1,5 @@
 export function freeze(content: DocumentFragment | Element, beHive: Element){
-    const decoratorElements = Array.from(beHive.children) as any;
+    const decoratorElements = Array.from(beHive.children) as any[];
     for(const decorEl of decoratorElements){
         const ifWantsToBe = (decorEl as any as Element).getAttribute('if-wants-to-be');
         if(ifWantsToBe === undefined) continue;
@@ -9,7 +9,15 @@ export function freeze(content: DocumentFragment | Element, beHive: Element){
         for(const el of converted){
             const currVal = decorEl.virtualPropsMap.get(el);
             el.removeAttribute(isAttr);
-            el.setAttribute(beAttr, JSON.stringify(currVal));
+            const vpm = decorEl.virtualPropsMap;
+            let attrVal = '';
+            if(vpm !== undefined){
+                const currVal = vpm.get(el);
+                attrVal = JSON.stringify(currVal);
+            }else{
+                console.warn('ploi');//potential loss of information
+            }
+            el.setAttribute(beAttr, attrVal);
         }
     }
 }
