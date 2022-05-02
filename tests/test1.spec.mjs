8c8bb500bc9f952ec306b01667f57eb7778d9969
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-02 19:27:35 -0400

    0.0.536

--- tests/test1.spec.mjs
+++ tests/test1.spec.mjs
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 test('test1', async ({ page }) => {
     await page.goto('./tests/xtal-editor-test1.html');
-    // wait for 1 second
+    // wait for 10 seconds
     await page.waitForTimeout(10000);
     const editor = page.locator('xtal-editor');
     await expect(editor).toHaveAttribute('mark', 'approved');
