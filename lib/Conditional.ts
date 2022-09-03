e73378f8944b22119ff1fd616ed77482372513ab
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-09-03 11:49:14 -0400

    0.0.582

--- lib/Conditional.ts
+++ lib/Conditional.ts
@@ -1,53 +1,38 @@
 import {RenderContext, IConditional} from './types';
 import {DTR} from './DTR';
-const prevCondition = new WeakMap<Element, Map<IConditional, boolean>>();
 export class Conditional{
     constructor(public dtr: DTR){}
     async do({host, rhs, ctx}: RenderContext): Promise<void> {
         const exp = rhs[1] as IConditional;
         const {getVal} = await import('./getVal.js');
-        let condition = true;
         if(exp.if !== undefined ){
             exp.ifVal = !!(await getVal(ctx!, exp.if));
         }
         if(exp.ifVal === false){
-            condition = false;
-            // await this.doFalse(ctx!);
-            // return;
-        }else{
-            if(exp.lhs !== undefined){
-                exp.lhsVal = await getVal(ctx!, exp.lhs);
-            }
-            if(exp.rhs !== undefined){
-                exp.rhsVal = await getVal(ctx!, exp.rhs);
-            }
-            
-            if(exp.lhsVal !== undefined || exp.rhsVal !== undefined){
-                switch(exp.op){
-                    case '===':
-                    case undefined:
-                        if(exp.lhsVal !== exp.rhsVal){
-                            condition = false;
-                        }
-                }
+            await this.doFalse(ctx!);
+            return;
+        }
+        if(exp.lhs !== undefined){
+            exp.lhsVal = await getVal(ctx!, exp.lhs);
+        }
+        if(exp.rhs !== undefined){
+            exp.rhsVal = await getVal(ctx!, exp.rhs);
+        }
+        let condition = true;
+        if(exp.lhsVal !== undefined || exp.rhsVal !== undefined){
+            switch(exp.op){
+                case '===':
+                case undefined:
+                    if(exp.lhsVal !== exp.rhsVal){
+                        condition = false;
+                    }
             }
         }
-        //TODO don't do either if expression matches previous value
-        const target = ctx!.target!;
-        let map = prevCondition.get(target);
-        if(map === undefined){
-            map = new Map<IConditional, boolean>();
-            prevCondition.set(target, map);
-        }
-        let prevVal = map.get(exp);
-        if(prevVal == condition) return;
-        map.set(exp, condition);
         if(condition){
             await this.doTrue(ctx!);
         }else{
             await this.doFalse(ctx!);
         }
-
 
     }
     async doTrue({host, rhs, ctx}: RenderContext){
