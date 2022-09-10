decabd326cd61c2a9aa56fa0c2911a2baaf5faa5
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-09-10 13:28:06 -0400

    0.0.590

--- lib/notifyHookup.ts
+++ lib/notifyHookup.ts
@@ -2,29 +2,24 @@ import {INotify, IMinimalNotify, EventSettings, INotifyHookupInfo} from './types
 export async function notifyHookup(target: Element, key: string, minSettings: IMinimalNotify): Promise<INotifyHookupInfo>{
     const isPropSet = key.endsWith(':onSet');
     const propName = isPropSet ?  key.substr(0, key.length - 6) : undefined;
-    //let handler:  ((event: Event) => Promise<void>) | undefined = undefined;
     let controller = new AbortController();
     const {doInit, eventListenerOptions, action} = minSettings;
     const minimal = action !== undefined;
-    if(doInit){
-        const {doAction} = await import ('./doAction.js');
-        if(minimal){
-            action();
-        }else{
-            const {getRecipientElement} = await import ('./getRecipientElement.js');
-            const recipientElement = await getRecipientElement(target,  minSettings as EventSettings);
-            if(recipientElement !== null) doAction(target, recipientElement,  minSettings as EventSettings);
-            if(isPropSet &&  target.localName.includes('-')){
-                await customElements.whenDefined(target.localName);
-            }
-        }
-    }
     const handler = async (e?: Event) => {
         const {doAction} = await import ('./doAction.js');
         const {getRecipientElement} = await import ('./getRecipientElement.js');
         const recipientElement = await getRecipientElement(target, minSettings as EventSettings);
         if(recipientElement !== null) doAction(target, recipientElement, minSettings as EventSettings);
     }
+    if(doInit){
+        const {doAction} = await import ('./doAction.js');
+        if(minimal){
+            await action();
+        }else{
+            await handler();
+        }
+    }
+
     if(isPropSet){
         const {bePropagating} = await import('./bePropagating.js');
         const et = await bePropagating(target, propName!);
