c0cc37e5b595e3f163f45fc602613c34f0e4a1b7
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-09-13 20:14:43 -0400

    0.0.600

--- lib/mixins/HomeIn.ts
+++ lib/mixins/HomeIn.ts
@@ -1,7 +1,5 @@
 import {TransformPacket, TemplMgmtBase} from '../types';
 
-export class HomeIn{
-    constructor(public base: TemplMgmtBase){
+export function HomeIn(base: TemplMgmtBase, fragment: DocumentFragment){
 
-    }
 }
