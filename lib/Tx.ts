64aa80eaa32fcf0dfd33b783b15c3736c60311f9
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-09-26 18:02:49 -0400

    0.0.610

--- lib/Tx.ts
+++ lib/Tx.ts
@@ -1,16 +1,15 @@
-import { RenderContext, MatchRHS} from 'trans-render/lib/types';
+import { RenderContext, MatchRHS, TransformScope} from 'trans-render/lib/types';
 import {ITx} from './types';
-import {getHost} from 'trans-render/lib/getHost.js';
 
 export class Tx implements ITx{
     #ctx: RenderContext
-    #realm: Element | ShadowRoot;
-    constructor(public host: any, public realmCitizen: Element, match: {[key: string]: MatchRHS}){
+    #realm: WeakRef<Element | ShadowRoot | DocumentFragment> | undefined;
+    constructor(host: any, public realmCitizen: Element, match: {[key: string]: MatchRHS}, public transformScope: TransformScope){
         // const target = pram.transformFromClosest !== undefined ?
         //     proxy.closest(pram.transformFromClosest)
         //     : host.shadowRoot || host!;
         const rn = (getHost(realmCitizen, true) || document) as HTMLElement;
-        this.#realm = rn!.shadowRoot || rn!;
+        //this.#realm = rn!.shadowRoot || rn!;
         this.#ctx = {
             match,
             host,
@@ -18,9 +17,33 @@ export class Tx implements ITx{
         };
     }
 
+    async #getRealm(){
+        if(this.#realm !== undefined){
+            const deref = this.#realm.deref();
+            if(deref !== undefined) return deref;
+        }
+        const {transformScope, realmCitizen} = this;
+        if(transformScope.parent){
+            const parent = realmCitizen.parentElement;
+            if(parent !== null){
+                this.#realm = new WeakRef(parent);
+                return parent;
+            }
+        }
+        const {closest} = transformScope;
+        if(closest !== undefined){
+            const nearest = realmCitizen.closest(closest);
+            return realmCitizen.closest(closest);
+        }
+        const {getHost} = await import('trans-render/lib/getHost.js');
+        const rn = (getHost(realmCitizen, true) || document) as Element | DocumentFragment;
+        this.#realm = new WeakRef(rn);
+        return rn;
+    }
+
     async transform(): Promise<void> {
         const {DTR} = await import('trans-render/lib/DTR.js');
-        await DTR.transform(this.#realm, this.#ctx);
+        await DTR.transform(this.#getRealm() as any as DocumentFragment, this.#ctx);
     }
 
 }
