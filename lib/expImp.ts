310287c4366a3a14acfef3ee150305f5a328fe2b
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-10-08 13:35:39 -0400

    0.0.619

--- lib/expImp.ts
+++ lib/expImp.ts
@@ -1,3 +1,19 @@
+
+/**
+ * "Expand" HTMLTemplateElement by replacing special tags with referenced templates
+ * @param templ 
+ * @param templRefs 
+ */
 export function expImp(templ: HTMLTemplateElement, templRefs: {[key: string]: HTMLTemplateElement}){
-    
+    const {content} = templ;
+    content.querySelectorAll('[bi]').forEach(bi => {
+        const localName = bi.localName;
+        const templ = templRefs[localName];
+        if(templ === undefined) return;
+        const clone = templ.content.cloneNode(true);
+        const parentElement = bi.parentElement;
+        if(parentElement !== null && bi.nextElementSibling === null){
+            parentElement.append(clone);
+        }
+    });
 }
