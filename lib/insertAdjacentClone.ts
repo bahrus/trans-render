17892450c1104d967deda6ff0c65f8ec4da32eb9
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-10-08 21:33:20 -0400

    0.0.620

--- lib/insertAdjacentClone.ts
+++ lib/insertAdjacentClone.ts
@@ -1,9 +1,25 @@
 export function insertAdjacentClone(clone: DocumentFragment, target: Element, position: InsertPosition){
     let appendTarget = target;
+    let lastTextNode: Node | undefined;
+    let prevAppendTarget: Element = appendTarget;
     let isFirst = true;
-    Array.from(clone.children).forEach(child =>{
+    const childNodes = Array.from(clone.childNodes);
+    for(const child of childNodes){
       const modifiedPosition : InsertPosition = isFirst ? position : 'afterend';
       isFirst = false;
-      appendTarget = appendTarget.insertAdjacentElement(modifiedPosition, child)!;
-    });
+      //assume for now alternates between element nodes and non element nodes
+      //surely there's a better way?
+      
+      if(child.nodeType === 1){
+        appendTarget = appendTarget.insertAdjacentElement(modifiedPosition, child as Element)!;
+        if(lastTextNode !== undefined){
+          prevAppendTarget.insertAdjacentText('afterend', lastTextNode.textContent!);
+          prevAppendTarget = appendTarget;
+          lastTextNode = undefined;
+        }
+      }else{
+         lastTextNode = child;
+      }
+    }
+    
 }
