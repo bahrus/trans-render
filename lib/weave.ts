2ce5d2d9c0a89f2186a7b6b11a14d91f5580bf95
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-11-10 20:00:24 -0500

    0.0.640

--- lib/weave.ts
+++ lib/weave.ts
@@ -1,5 +1,5 @@
 export async function weave(textNodes: string[], host: any){
-    (await Promise.all(textNodes.map(async (path, idx) => {
+    return (await Promise.all(textNodes.map(async (path, idx) => {
         if(idx % 2 === 0) return path;
         const {getVal} = await import ('./getVal.js');
         return await getVal({host}, path) as string;
