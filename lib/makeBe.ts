7fc0111a55c0a7a859da1532c38b5b91be9ee7f0
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-12-15 23:52:03 -0500

    0.0.650

--- lib/makeBe.ts
+++ lib/makeBe.ts
@@ -17,7 +17,7 @@ export function makeItBe(instance: Element, cssSelector: string, make: CSSSelect
         const aInstance = instance as any;
         if(aInstance.beDecorated === undefined) aInstance.beDecorated = {};
         const attrib = instance.getAttribute(wcName);
-        const having2  = {...having} || {};
+        const having2  = {...having || {}};
         if(attrib && attrib.startsWith('{')){
             const val = JSON.parse(attrib);
             Object.assign(having2, val);
@@ -25,7 +25,8 @@ export function makeItBe(instance: Element, cssSelector: string, make: CSSSelect
         aInstance.beDecorated[be] = having2;
         if(customElements.get(wcName)){
             const dem = document.createElement(wcName) as any as Attachable;
-            dem.attach.bind(instance);
+            //dem.attach.bind(dem);
+            dem.attach(instance);
         }else{
             instance.setAttribute(wcName, '');
         }
