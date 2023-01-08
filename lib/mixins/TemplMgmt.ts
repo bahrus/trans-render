1c38b320a55f1c913d04da1d7575f6578e40c8dd
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-01-08 09:19:36 -0500

    0.0.658

--- lib/mixins/TemplMgmt.ts
+++ lib/mixins/TemplMgmt.ts
@@ -22,12 +22,6 @@ export const TemplMgmt = (superclass: TemplMgmtBaseMixin) => class extends super
             
     }
     async cloneTemplate({noshadow, shadowRoot, mainTemplate, mntCnt, hydratingTransform, transform}: TemplMgmtBase){
-        // if(mntCnt === undefined){
-        //     let count = 0;
-        //     if(hydratingTransform !== undefined) count++;
-        //     if(transform !== undefined) count++;
-        //     this.mntCnt = count;
-        // }
         let root = this as any;
         if(!noshadow){
             if(shadowRoot === null){
