14f18f418b663766d1b4c4b2b8ca4a383f423605
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-01-10 19:31:30 -0500

    0.0.659

---
+++ lib/resolve.ts
@@ -0,0 +1,25 @@
+let imports: any;
+//const lowerCaseRe = /^[a-zA-Z]/;
+
+export function resolve(href: string){
+    let finalURL = href!;
+    const linkTest = (<any>globalThis)[finalURL];
+    if(linkTest instanceof HTMLLinkElement){
+        return linkTest.href;
+        return finalURL;
+    }
+    const importMap = document.querySelector('script[type="importmap"]');
+    if(importMap !== null){
+        try{
+            if(imports === undefined){
+                imports = JSON.parse(importMap.innerHTML).imports;
+            }
+            for(const key in imports){
+                if(finalURL.startsWith(key)){
+                   return finalURL.replace(key, imports[key]);
+                }
+            }
+        }catch(e){}
+    }
+   throw href + ' not resolved by import maps or link tag.'   
+}
