c12a3f0553892e3c8b57b931c4dae7aa97c1e260
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-01-20 04:51:46 -0500

    0.0.663

--- lib/getAdjacentChildren.ts
+++ lib/getAdjacentChildren.ts
@@ -1,3 +1,12 @@
 export function getAdjacentChildren(templ: HTMLTemplateElement){
-    throw 'NI';
+    const children: Element[] = [];
+    const cnt = Number(templ.dataset.cnt);
+    let idx = 1;
+    let ns: Node | null = templ;
+    while(idx < cnt && ns !== null){
+        ns = ns.nextSibling;
+        if(ns instanceof Element) children.push(ns);
+        idx++;
+    }
+    return children;
 }
