a454bfbcfd81b68a557a7fabf2e605a40e4233d8
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-01-23 20:34:14 -0500

    0.0.664

--- lib/mixins/Join.ts
+++ lib/mixins/Join.ts
@@ -1,4 +1,4 @@
-import {Transformer, TransformJoinEvent} from '../types';
+import {Matches, MatchRHS, RHS, Transformer, TransformJoinEvent} from '../types';
 export class Join{
     constructor(public transformer: Transformer, fragment: DocumentFragment){
         const childElements = Array.from(fragment.children);
@@ -8,7 +8,8 @@ export class Join{
                 if(detail !== undefined){
                     const {match} = detail as TransformJoinEvent;
                     if(match !== undefined){
-                        Object.assign(transformer.ctx, match);
+                        const newMatch = join(transformer.ctx.match!, match);
+                        transformer.ctx.match! = newMatch;
                     }
                 }
                 const {target} = e;
@@ -17,4 +18,48 @@ export class Join{
             });
         }
     }
+}
+
+function join(lhs: Matches, rhs: Matches): Matches{
+    const lhsGrouped = group(lhs);
+    const rhsGrouped = group(rhs);
+    const returnObj: Matches = {};
+    for(const key in lhsGrouped){
+        let val = lhsGrouped[key];
+        const addend = rhsGrouped[key];
+        if(addend !== undefined){
+            val = val.concat(addend);
+        }
+        let count  = 0;;
+        let dupKey = '^';
+        for(const m of val){
+            if(count === 0){
+                returnObj[key] = m;
+            }else{
+                returnObj[dupKey] = m;
+            }
+            count++;
+            dupKey = '^' + count;
+        }
+    }
+    return returnObj;
+}
+
+function group(match: Matches): {[key: string]: RHS[]}{
+    const returnObj: {[key: string]: RHS[]} = {};
+    let lastKey = '';
+    for(const key in match){
+        let realKey = key;
+        if(key[0] === '^'){
+            realKey = lastKey;
+        }else{
+            lastKey = key;
+        }
+        if(returnObj[realKey] === undefined){
+            returnObj[realKey] = [];
+        }
+        const rhs = match[realKey];
+        returnObj[key].push(rhs)
+    }
+    return returnObj;
 }
