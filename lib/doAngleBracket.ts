c5afc3f68564a35e319f26a0500859fc1d205eac
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-01-27 20:48:41 -0500

    0.0.662

--- lib/doAngleBracket.ts
+++ lib/doAngleBracket.ts
@@ -2,9 +2,13 @@ import { RenderContext, Transformer } from "./types";
 
 export async function doAngleBracket(ctx: RenderContext, sup: (ctx: RenderContext) => void){
     const {target, host, rhs} = ctx;
-    const method = host[rhs];
+    const dp = new DOMParser();
+    const doc = dp.parseFromString(rhs, 'text/html'); //switch to setHTML eventually, when no sanitizing, I guess
+    const firstChild = doc.body.firstElementChild!;
+    const methodName = `<${firstChild.localName}/>`
+    const method = host[methodName];
     if(typeof method === 'function'){
-        await method(ctx);
+        await method(ctx, doc);
     }else{
         sup(ctx);
     }
