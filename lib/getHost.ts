062c7b218650b9af51a6cc3a0e6cabe11fc59084
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-02-02 06:06:45 -0500

    0.0.662

--- lib/getHost.ts
+++ lib/getHost.ts
@@ -1,9 +1,13 @@
-export function getHost(self:Element, tocoho?: boolean | string): Element | null{
+export function getHost(self:Element, tocoho?: boolean | string): EventTarget | null{
     let host: Element | null | undefined;
     if(tocoho){
         const closest = tocoho === true ? '[itemscope]' : tocoho;
         host = (self.parentElement || self).closest(closest);
-        if(host) return host;
+        if(host) {
+            const scope = (<any>host)?.beDecorated?.scoped?.scope as EventTarget;
+            if(scope != undefined) return scope;
+            return host;
+        }
     }
     host = (<any>self.getRootNode()).host;
     if(host === undefined){
--- lib/subscribe.ts
+++ lib/subscribe.ts
@@ -1,5 +1,5 @@
 import {IPropChg} from '../froop/types';
-export type Callback = (element: Element, propName: string, newValue: any) => void;
+export type Callback = (element: EventTarget, propName: string, newValue: any) => void;
 
 const propSubscribers = new WeakMap<Element, {[key: string]: Callback[]}>();
 
@@ -7,16 +7,25 @@ export function tooSoon(element: Element){
     return element.localName.includes('-') && customElements.get(element.localName) === undefined;
 }
 
-export async function subscribe(element: Element, propName: string, callback: Callback) {
+function subscribeToPropagator(propagator: EventTarget, propName: string, callback: Callback){
+    propagator.addEventListener(propName, e => {
+        const pc = (e as CustomEvent).detail as IPropChg;
+        callback(propagator, propName, pc.newVal);
+    });
+}
+
+export async function subscribe(element: EventTarget, propName: string, callback: Callback) {
+    if( (<any>element)._isPropagating){
+        subscribeToPropagator(element, propName, callback);
+        return;
+    }
+    if(!(element instanceof Element)) throw 'NI';
     if(tooSoon(element)){
         await customElements.whenDefined(element.localName);
     }
     const propagator = (<any>element.constructor)?.ceDef?.services?.propper?.stores?.get(element) as EventTarget;
     if(propagator !== undefined){
-        propagator.addEventListener(propName, e => {
-            const pc = (e as CustomEvent).detail as IPropChg;
-            callback(element, propName, pc.newVal);
-        });
+        subscribeToPropagator(propagator, propName, callback);
         return;
     }
     if(!propSubscribers.has(element)){
