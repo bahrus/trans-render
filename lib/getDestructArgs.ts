cfdfa1083120634d6203b3a9a8201ce4e9a0fc03
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-02-03 22:39:21 -0500

    0.0.662

--- lib/getDestructArgs.ts
+++ lib/getDestructArgs.ts
@@ -5,7 +5,7 @@ export function getDestructArgs(fn: Function){
 export function getArgsFromString(str: String){
     const fnString = str.trim();
     if(fnString.startsWith('({')){
-        const iPos = fnString.indexOf('})', 2);
+        const iPos = fnString.indexOf('}', 2);
         return fnString.substring(2, iPos).split(',').map(s => substrBefore(s, ':'));
     }else{
         return [];
