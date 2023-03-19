acd9f4958212b6715d178b928ac6de28d77e7d1f
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-03-19 12:39:46 -0400

    0.0.670

--- lib/doBeHavings.ts
+++ lib/doBeHavings.ts
@@ -14,7 +14,7 @@ export async function doBeHavings(instance: Element, beHavings: BeHaving[]){
             Object.assign(having2, val);
         }
         const camelBe = lispToCamel(be);
-        const existingSettings = aInstance.beDecorated[camelBe];
+        let existingSettings = aInstance.beDecorated[camelBe];
         let alreadyAttached = false;
         if(existingSettings !== undefined){
             alreadyAttached = existingSettings.controller !== undefined;
@@ -33,6 +33,8 @@ export async function doBeHavings(instance: Element, beHavings: BeHaving[]){
         if(waitForResolved){
             if(existingSettings !== undefined && existingSettings.resolved) continue;
             const {isResolved} = await import('./isResolved.js');
+            existingSettings = aInstance.beDecorated[camelBe];
+            if(existingSettings !== undefined && existingSettings.resolved) continue;
             await isResolved(instance, ['beDecorated', be]);
         }
 
