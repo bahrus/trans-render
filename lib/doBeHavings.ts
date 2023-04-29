5bc1892c67574a3810b0559c42a97d2c6d357cc0
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-04-29 16:02:02 -0400

    0.0.676

--- lib/doBeHavings.ts
+++ lib/doBeHavings.ts
@@ -2,41 +2,51 @@ import {Attachable, BeHaving} from './types';
 import { lispToCamel } from './lispToCamel.js';
 
 export async function doBeHavings(instance: Element, beHavings: BeHaving[]){
+    await customElements.whenDefined('be-enhanced');
     for(const beHaving of beHavings){
         const {be, having, waitForResolved} = beHaving;
-        const wcName = 'be-' + be;
+        const enh = 'be-' + be;
+        const enhancement = lispToCamel(enh);
         const aInstance = instance as any;
-        if(aInstance.beDecorated === undefined) aInstance.beDecorated = {};
-        const attrib = instance.getAttribute(wcName);
-        const having2  = {...having || {}};
-        if(attrib && attrib.startsWith('{')){
-            const val = JSON.parse(attrib);
-            Object.assign(having2, val);
+        if(having !== undefined){
+            const base = (<any>instance).beEnhanced.by[enhancement];
+            Object.assign(base, {...having});
         }
-        const camelBe = lispToCamel(be);
-        let existingSettings = aInstance.beDecorated[camelBe];
-        let alreadyAttached = false;
-        if(existingSettings !== undefined){
-            alreadyAttached = existingSettings.controller !== undefined;
-            Object.assign(existingSettings, having2);
-        }else{
-            aInstance.beDecorated[camelBe] = having2;
+        if(waitForResolved){
+            await (<any>instance).beEnhanced.whenDefined(enh);
         }
         
-        if(!alreadyAttached){
-            await customElements.whenDefined(wcName);
-            const decorator = document.createElement(wcName) as any as Attachable;
-            decorator.attach(instance);
+        // if(aInstance.beDecorated === undefined) aInstance.beDecorated = {};
+        // const attrib = instance.getAttribute(wcName);
+        // const having2  = {...having || {}};
+        // if(attrib && attrib.startsWith('{')){
+        //     const val = JSON.parse(attrib);
+        //     Object.assign(having2, val);
+        // }
+        // const camelBe = lispToCamel(be);
+        // let existingSettings = aInstance.beDecorated[camelBe];
+        // let alreadyAttached = false;
+        // if(existingSettings !== undefined){
+        //     alreadyAttached = existingSettings.controller !== undefined;
+        //     Object.assign(existingSettings, having2);
+        // }else{
+        //     aInstance.beDecorated[camelBe] = having2;
+        // }
+        
+        // if(!alreadyAttached){
+        //     await customElements.whenDefined(wcName);
+        //     const decorator = document.createElement(wcName) as any as Attachable;
+        //     decorator.attach(instance);
 
-        }
+        // }
 
-        if(waitForResolved){
-            if(existingSettings !== undefined && existingSettings.resolved) continue;
-            const {isResolved} = await import('./isResolved.js');
-            existingSettings = aInstance.beDecorated[camelBe];
-            if(existingSettings !== undefined && existingSettings.resolved) continue;
-            await isResolved(instance, ['beDecorated', be]);
-        }
+        // if(waitForResolved){
+        //     if(existingSettings !== undefined && existingSettings.resolved) continue;
+        //     const {isResolved} = await import('./isResolved.js');
+        //     existingSettings = aInstance.beDecorated[camelBe];
+        //     if(existingSettings !== undefined && existingSettings.resolved) continue;
+        //     await isResolved(instance, ['beDecorated', be]);
+        // }
 
     }
 }
