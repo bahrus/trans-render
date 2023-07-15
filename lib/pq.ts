c2709fe4a5ce1e17c2ba8cc0753aee3e03e63942
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-07-15 11:06:25 -0400

    0.0.695

--- lib/pq.ts
+++ lib/pq.ts
@@ -1,21 +1,25 @@
 import {LogicOp, ListOfLogicalExpressions} from './types';
+import {all} from './all.js';
+import {none} from './none.js';
+import {eq} from './eq.js';
+import {oneOf} from './oneOf.js';
 export async function pq<MCProps = any>(expr: LogicOp<any>, src: MCProps): Promise<boolean>{
     const {ifAllOf, ifNoneOf, ifEquals, ifAtLeastOneOf} = expr;
     if(ifAllOf !== undefined){
-        const {all} = await import('./all.js');
-        if(!await all(ifAllOf as ListOfLogicalExpressions, src)) return false;
+        //const {all} = await import('./all.js');
+        if(!all(ifAllOf as ListOfLogicalExpressions, src)) return false;
     }
     if(ifNoneOf !== undefined){
-        const {none} = await import('./none.js');
-        if(!await none(ifNoneOf as ListOfLogicalExpressions, src)) return false;
+        //const {none} = await import('./none.js');
+        if(!none(ifNoneOf as ListOfLogicalExpressions, src)) return false;
     }
     if(ifEquals !== undefined){
-        const {eq} = await import('./eq.js');
-        if(!await eq(ifEquals as ListOfLogicalExpressions, src)) return false;
+        //const {eq} = await import('./eq.js');
+        if(!eq(ifEquals as ListOfLogicalExpressions, src)) return false;
     }
     if(ifAtLeastOneOf !== undefined){
-        const {oneOf} = await import('./oneOf.js');
-        if(!await oneOf(ifAtLeastOneOf as ListOfLogicalExpressions, src)) return false;
+        //const {oneOf} = await import('./oneOf.js');
+        if(!oneOf(ifAtLeastOneOf as ListOfLogicalExpressions, src)) return false;
     }
     return true;
 }
