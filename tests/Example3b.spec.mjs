380b30bfe8922d3087688d608686c3d69577cc60
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-12-28 09:25:00 -0500

    0.0.716

--- tests/Example3b.spec.mjs
+++ tests/Example3b.spec.mjs
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Example3b', async ({ page }) => {
   await page.goto('./tests/Example3b.html');
   // wait for 1 second
-  await page.waitForTimeout(6000);
+  await page.waitForTimeout(2000);
   const editor = page.locator('#target');
   await expect(editor).toHaveAttribute('mark', 'good');
 });
