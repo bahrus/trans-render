ba088199b954aaebaaf47db79d2fa09ed99a16b2
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-12-29 09:30:02 -0500

    0.0.718

---
+++ tests/Example5c.spec.mjs
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('Example5c', async ({ page }) => {
+  await page.goto('./tests/Example5c.html');
+  // wait for 1 second
+  await page.waitForTimeout(2000);
+  const editor = page.locator('#target');
+  await expect(editor).toHaveAttribute('mark', 'good');
+});
---
+++ tests/Example5d.js
@@ -0,0 +1,22 @@
+import { Transform } from '../Transform.js';
+const model = {
+    isHappy: false,
+    handleInput: (e, { model, propagator }) => {
+        model.isHappy = !model.isHappy;
+        propagator?.dispatchEvent(new Event('isHappy'));
+    }
+};
+const form = document.querySelector('form');
+const propagator = new EventTarget();
+Transform(form, model, {
+    input: {
+        a: ['handleInput', {
+                on: 'change',
+                do: (e, { model, propagator }) => {
+                    model.isHappy = !model.isHappy;
+                    propagator?.dispatchEvent(new Event('isHappy'));
+                }
+            }]
+    },
+    span: 'isHappy'
+}, propagator);
