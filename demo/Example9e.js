da39148badc657ffffe126e0d6513cdf8b82acea
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-02-01 05:02:11 -0500

    0.0.733

---
+++ demo/Example9e.js
@@ -0,0 +1,35 @@
+import { Transform } from '../Transform.js';
+const model = {
+    list: [
+        {
+            myProp: 'row 1'
+        },
+        {
+            myProp: 'row 2'
+        }
+    ]
+};
+const div = document.querySelector('div');
+Transform(div, model, {
+    '$ list': {
+        f: {
+            each: 0,
+            clone: '[aria-rowindex="0"]',
+            xform: {
+                '| myProp': 0
+            },
+            appendTo: 'tbody',
+            indexProp: 'ariaRowIndex',
+            timestampProp: 'myProp',
+        }
+    }
+});
+setTimeout(() => {
+    console.log('update model');
+    const list = [
+        {
+            myProp: 'row 4'
+        },
+    ];
+    model.list = list;
+}, 2000);
