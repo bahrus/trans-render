99d8f721b00980239a488c7f3da0d985745f1c70
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-03-29 18:28:35 -0400

    0.0.755

--- dss/findR.js
+++ dss/findR.js
@@ -1,18 +1,34 @@
 export async function findR(element, specifier, scopeE) {
     const { scopeS, elS } = specifier;
     if (scopeS !== undefined && elS !== undefined) {
-        const { dss, rec, rnf } = specifier;
+        const { dss, rec, rnf, host } = specifier;
         switch (dss) {
             case '^':
                 const { parentElement } = (scopeE || element);
                 const closest = parentElement?.closest(scopeS);
+                if (host && closest) {
+                    const { localName } = closest;
+                    if (localName.includes('-')) {
+                        await customElements.whenDefined(localName);
+                        return closest;
+                    }
+                }
                 const found = closest?.querySelector(elS);
-                if (found)
+                if (found) {
                     return found;
+                }
                 if (rec && closest)
                     return await findR(element, specifier, closest);
                 if (rnf) {
                     const rn = element.getRootNode();
+                    if (host && rn instanceof ShadowRoot) {
+                        const h = rn.host;
+                        const { localName } = h;
+                        if (localName.includes('-')) {
+                            await customElements.whenDefined(localName);
+                            return h;
+                        }
+                    }
                     return rn?.querySelector(elS);
                 }
                 return null;
