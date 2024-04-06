7f7dd4fd5f8ba2274c46fb4524d4f5522457164e
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-04-06 19:00:50 -0400

    Merge branch 'baseline' of https://github.com/bahrus/trans-render into baseline

--- froop/O.js
+++ froop/O.js
@@ -114,6 +114,8 @@ export class O extends HTMLElement {
                         this[publicPrivateStore][key] = nv;
                         const customState = prop.css;
                         if (customState !== undefined) {
+                            //wrong!  we cannot do any special logic inside the setter because being roundabout ready means 
+                            //setters avoided often in favor of just dispatching events.
                             const customStateObj = typeof customState === 'string' ? {
                                 nameValue: customState,
                             } : customState;
