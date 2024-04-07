e20a318aad542fa137e1e80e090f35badc9849fe
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-04-07 13:36:17 -0400

    Merge branch 'baseline' of https://github.com/bahrus/trans-render into baseline

--- froop/types.d.ts
+++ froop/types.d.ts
@@ -186,6 +186,9 @@ export interface OConfig<TProps = any, TActions = TProps, ETProps = TProps>{
     onsets?: Onsets<TProps, TActions>;
     actions?: Actions<TProps, TActions>;
     propChangeMethod?: keyof TActions;
+    /**
+     * inferred actions
+     */
     infractions?: Infractions<TProps>,
     //style?: Partial<CSSStyleDeclaration>;
     compacts?: Compacts<TProps, TActions, ETProps>;
