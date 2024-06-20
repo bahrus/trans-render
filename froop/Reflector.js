b69c2c81ee7f2987c2b40544419d82305163a9e6
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-06-19 20:03:24 -0400

    0.0.783

--- froop/Reflector.js
+++ froop/Reflector.js
@@ -1,18 +1,30 @@
 export class Reflector {
+    #acs;
     constructor(instance, attrsToReflect) {
         const { propagator } = instance;
+        this.#acs = [];
         const attrs = instance.constructor.attrs;
         const parsedAttrsToReflect = attrsToReflect.split(' ');
         for (const attr in attrs) {
             if (!parsedAttrsToReflect.includes(attr))
                 continue;
+            const ac = new AbortController();
+            this.#acs.push(ac);
             const propInfo = attrs[attr];
             const { propName } = propInfo;
             propagator.addEventListener(propName, e => {
                 console.log({ e });
                 this.reflect(instance, attr, propName, propInfo);
-            });
+            }, { signal: ac.signal });
             this.reflect(instance, attr, propName, propInfo);
+        }
+        propagator.addEventListener('disconnectedCallback', e => {
+            this.#disconnect();
+        });
+    }
+    #disconnect() {
+        for (const ac of this.#acs) {
+            ac.abort();
         }
     }
     reflect(instance, attr, propName, propInfo) {
--- froop/Reflector.ts
+++ froop/Reflector.ts
@@ -2,19 +2,32 @@ import {O} from './O.js';
 import { PropInfo } from './types';
 
 export class Reflector{
+    #acs: AbortController[];
     constructor(instance: O, attrsToReflect: string){
         const {propagator} = instance;
+        this.#acs = [];
         const attrs = (<any>instance.constructor).attrs as {[key: string] : PropInfo};
         const parsedAttrsToReflect = attrsToReflect.split(' ');
         for(const attr in attrs){
             if(!parsedAttrsToReflect.includes(attr)) continue;
+            const ac: AbortController = new AbortController();
+            this.#acs.push(ac);
             const propInfo = attrs[attr];
             const {propName} = propInfo;
             propagator.addEventListener(propName!, e => {
                 console.log({e});
                 this.reflect(instance, attr, propName!, propInfo);
-            });
+            }, {signal: ac.signal});
             this.reflect(instance, attr, propName!, propInfo);
+        }
+        propagator.addEventListener('disconnectedCallback', e => {
+            this.#disconnect();
+        })
+    }
+
+    #disconnect(){
+        for(const ac of this.#acs){
+            ac.abort();
         }
     }
 
