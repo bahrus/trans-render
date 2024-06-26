ea3bdd8a325a002555edd873631656231d09d63b
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-06-26 07:07:30 -0400

    0.0.786

--- froop/CustStExt.js
+++ froop/CustStExt.js
@@ -8,6 +8,7 @@ export class CustStExt {
         this.#do(instance, internals);
     }
     async #do(instance, internals) {
+        const { propagator } = instance;
         for (const statement of this.splitSplit) {
             const [customStateKey, expr] = statement;
             const re = new RegExp(String.raw `^(?<lhs>.*)(?<op>==|>|>=|<|<=)(?<rhs>.*)`);
@@ -16,9 +17,35 @@ export class CustStExt {
             const parsedExpr = {
                 lhs: groups.lhs.trim(),
                 op: groups.op,
-                rhs: groups.rhs.trim()
+                rhs: groups.rhs.trim(),
             };
+            const re2 = new RegExp(String.raw `^(?<prop>[\w]+)`);
+            if (re2 !== null) {
+                const propName = parsedExpr.lhs;
+                propagator.addEventListener(propName, e => {
+                    this.#simpleCompare(instance, internals, parsedExpr, propName, customStateKey);
+                });
+                this.#simpleCompare(instance, internals, parsedExpr, propName, customStateKey);
+            }
             console.log({ parsedExpr, test, expr });
+        }
+    }
+    #simpleCompare(instance, internals, parsedExpr, propName, customStateKey) {
+        const val = instance[propName];
+        const { op, rhs } = parsedExpr;
+        //let method : 'add' | 'delete' = 'delete';
+        switch (op) {
+            case '==': {
+                if (val === null || val === undefined) {
+                    internals.states.delete(customStateKey);
+                    return;
+                }
+                const method = val.toString() === rhs ? 'add' : 'delete';
+                internals.states[method](customStateKey);
+                return;
+            }
+            default:
+                throw 'NI';
         }
     }
 }
--- froop/CustStExt.ts
+++ froop/CustStExt.ts
@@ -8,6 +8,7 @@ export class CustStExt {
     }
 
     async #do(instance: O, internals: ElementInternals){
+        const {propagator} = instance;
         for(const statement of this.splitSplit){
             const [customStateKey, expr] = statement;
             const re = new RegExp(String.raw `^(?<lhs>.*)(?<op>==|>|>=|<|<=)(?<rhs>.*)`);
@@ -17,10 +18,47 @@ export class CustStExt {
             const parsedExpr = {
                 lhs: groups.lhs.trim(),
                 op: groups.op,
-                rhs: groups.rhs.trim()
-            };
+                rhs: groups.rhs.trim(),
+            } as ParsedExpr;
+            const re2 = new RegExp(String.raw `^(?<prop>[\w]+)`);
+            if(re2 !== null){
+                const propName = parsedExpr.lhs;
+                propagator.addEventListener(propName, e => {
+                    this.#simpleCompare(instance, internals, parsedExpr, propName, customStateKey);
+                });
+                this.#simpleCompare(instance, internals, parsedExpr, propName, customStateKey);
+            }
             console.log({parsedExpr, test, expr})
         }
     }
+
+    #simpleCompare(
+        instance: O, internals: ElementInternals, 
+        parsedExpr: ParsedExpr, propName: string,
+        customStateKey: string,
+    ){
+        const val = (<any>instance)[propName!];
+        const {op, rhs} = parsedExpr;
+        //let method : 'add' | 'delete' = 'delete';
+        switch(op){
+            case '==':{
+                if(val === null || val === undefined){
+                    (<any>internals).states.delete(customStateKey);
+                    return;
+                }
+                const method = val.toString() === rhs ? 'add' : 'delete';
+                (<any>internals).states[method](customStateKey);
+                return;
+            }
+            default:
+                throw 'NI';
+        }
+    }
     
+}
+
+interface ParsedExpr {
+    lhs: string,
+    op: '==' | '<' | '<=' | '>=' | '>',
+    rhs: string,
 }
