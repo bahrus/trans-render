93c86f1e10677a64acbfbd7837a01740940f4f56
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-04 09:09:47 -0400

    0.0.820

--- lib/insertTempl.ts
+++ lib/insertTempl.ts
@@ -16,3 +16,13 @@ export function insertTempl(templ: HTMLTemplateElement, baseID: string){
     if(!templ.hasAttribute('itemscope')) templ.setAttribute('itemscope', '');
     templ.after(clone);
 }
+export function getDep(templ: HTMLTemplateElement, refs: string){
+    const rn = templ.getRootNode() as DocumentFragment;
+    const keys = refs
+        .split(' ')
+        .map(s => s.trim())
+        .filter(s => !!s)
+        .map(key => rn.getElementById(key))
+        .filter(x => x !== null)
+    ;
+}
