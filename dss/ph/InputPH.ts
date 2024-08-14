7adc721d445a37f7e3347f6351f49725733327b9
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-13 20:50:46 -0400

    0.0.825

---
+++ dss/ph/InputPH.ts
@@ -0,0 +1,34 @@
+import { PHI, GetPHOptions } from "../../ts-refs/trans-render/dss/types";
+
+/**
+ * Prop Host
+ */
+export class InputPH<TValue, HTMLInputElement> implements PHI<TValue, HTMLInputElement>{
+
+    constructor(public options: GetPHOptions, element: HTMLInputElement){
+        
+    }
+    propagator: EventTarget;
+    getValue(el: HTMLInputElement): Promise<TValue | undefined> {
+        throw new Error("Method not implemented.");
+    }
+    setValue(el: HTMLInputElement, val: TValue) {
+        throw new Error("Method not implemented.");
+    }
+    hydrate(el: HTMLInputElement) {
+        throw new Error("Method not implemented.");
+    }
+    syncVal(el: HTMLInputElement) {
+        throw new Error("Method not implemented.");
+    }
+    disconnect() {
+        throw new Error("Method not implemented.");
+    }
+    toString(nv: TValue): string {
+        throw new Error("Method not implemented.");
+    }
+    outEvtName = 'value';
+    handleEvent(object: Event): void {
+        throw new Error("Method not implemented.");
+    }
+}
--- dss/ph/getPH.ts
+++ dss/ph/getPH.ts
@@ -10,25 +10,24 @@ if(map === undefined){
 }
 
 export async function getPH(element: Element, options: GetPHOptions){
-    if(map.has(element)) return;
-    let {isRoundAboutReady, prop, evtName} = options;
-
-    if(isRoundAboutReady === undefined){
-        options.isRoundAboutReady = isRoundAboutReady =  (<any>element).propagator instanceof EventTarget;
-    }
-    if(isRoundAboutReady && prop === undefined){
-        if(prop === undefined){
-            options.prop = prop = 'value';
-        }if(evtName === undefined){
-            options.evtName = prop;
+    if(!map.has(element)){
+        let {isRoundAboutReady, prop, evtName} = options;
+        if(isRoundAboutReady === undefined){
+            options.isRoundAboutReady = isRoundAboutReady =  (<any>element).propagator instanceof EventTarget;
         }
-        const {RA_PH} = await import('./RA_PH.js');
-        map.set(element, new RA_PH(options, element as any as  RoundaboutReady))
+        if(isRoundAboutReady && prop === undefined){
+            if(prop === undefined){
+                options.prop = prop = 'value';
+            }if(evtName === undefined){
+                options.evtName = prop;
+            }
+            const {RA_PH} = await import('./RA_PH.js');
+            if(!map.has(element)) map.set(element, new RA_PH(options, element as any as  RoundaboutReady));
+        }else if(element instanceof HTMLInputElement){
+            const {InputPH} = await import('./InputPH.js');
+            if(!map.has(element)) map.set(element, new InputPH(options, element));
+        }
     }
+    if(!map.has(element)) throw 'NI';
+    return map.get(element)!
 }
-
-// export function getPH(element: Element){
-//     if(!map.has(element)){
-//         map.set(element, new PH());
-//     }
-// }
