f72eb1648f72f9d0a3773b453201adc3b9ab3c05
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-22 07:36:58 -0400

    0.0.825

--- asmr/share/Std.js
+++ asmr/share/Std.js
@@ -1,3 +1,4 @@
+import { StMr } from '../StMr.js';
 /**
  * Flow Content Container
  */
@@ -5,55 +6,52 @@ export class Std {
     so;
     constructor(targetEl, so) {
         this.so = so;
-        this.mindRead(targetEl, so);
+        //this.mindRead(targetEl, so);
+        StMr(targetEl, so);
     }
-    mindRead(targetEl, so) {
-        let { valueProp, valueType, displayProp } = so;
-        const { localName } = targetEl;
-        if (valueProp === undefined) {
-            if (valueType === 'Boolean') {
-                if ('checked' in targetEl) {
-                    valueProp = 'checked';
-                }
-                else {
-                    valueProp = 'ariaChecked';
-                }
-            }
-            else {
-                if ('value' in targetEl && !'button-li'.includes(localName)) { //example 'input', 'output'
-                    valueProp = 'value';
-                }
-                else if ('href' in targetEl) { //example 'a', 'link'
-                    valueProp = 'href';
-                }
-                else {
-                    switch (valueType) {
-                        case 'NumericRange':
-                            valueProp = 'ariaValueNow';
-                            break;
-                    }
-                }
-            }
-            so.valueProp = valueProp;
-        }
-        if (displayProp === undefined) {
-            switch (localName) {
-                case 'input':
-                    //no value
-                    break;
-                default:
-                    switch (valueType) {
-                        case 'NumericRange':
-                            displayProp = 'ariaValueText';
-                            break;
-                        default:
-                            displayProp = 'textContent';
-                            break;
-                    }
-            }
-            so.displayProp = displayProp;
-        }
-    }
+    // mindRead(targetEl: Element, so: SetOptions){
+    //     let {valueProp, valueType, displayProp} = so;
+    //     const {localName} = targetEl;
+    //     if(valueProp === undefined){
+    //         if(valueType === 'Boolean'){
+    //             if('checked' in targetEl){
+    //                 valueProp = 'checked';
+    //             }else{
+    //                 valueProp = 'ariaChecked';
+    //             }
+    //         }else{
+    //             if('value' in targetEl && !'button-li'.includes(localName)){ //example 'input', 'output'
+    //                 valueProp = 'value';
+    //             }else if('href' in targetEl){ //example 'a', 'link'
+    //                 valueProp = 'href';
+    //             }else{
+    //                 switch(valueType){
+    //                     case 'NumericRange':
+    //                         valueProp = 'ariaValueNow';
+    //                         break;
+    //                 }
+    //             }
+    //         }
+    //         so.valueProp = valueProp;
+    //     }
+    //     if(displayProp === undefined){
+    //         switch(localName){
+    //             case 'input':
+    //                 //no value
+    //                 break;
+    //             default:
+    //                 switch(valueType){
+    //                     case 'NumericRange':
+    //                         displayProp = 'ariaValueText';
+    //                         break;
+    //                     default:
+    //                         displayProp = 'textContent';
+    //                         break;
+    //                 }
+    //         }
+    //         so.displayProp = displayProp;
+    //     }
+    // }
     #pureValue;
     async setValue(el, val) {
         this.#pureValue = val;
--- asmr/share/Std.ts
+++ asmr/share/Std.ts
@@ -1,61 +1,62 @@
 import { SetOptions, SharingObject, ValueProp } from "../../ts-refs/trans-render/asmr/types";
-
+import { StMr } from '../StMr.js';
 
 /**
  * Flow Content Container
  */
 export class Std<TProp = any> implements SharingObject{
     constructor(targetEl: Element, public so: SetOptions){
-        this.mindRead(targetEl, so);
+        //this.mindRead(targetEl, so);
+        StMr(targetEl, so);
     }
-    mindRead(targetEl: Element, so: SetOptions){
-        let {valueProp, valueType, displayProp} = so;
-        const {localName} = targetEl;
-        if(valueProp === undefined){
-            if(valueType === 'Boolean'){
-                if('checked' in targetEl){
-                    valueProp = 'checked';
-                }else{
-                    valueProp = 'ariaChecked';
-                }
-            }else{
-                if('value' in targetEl && !'button-li'.includes(localName)){ //example 'input', 'output'
-                    valueProp = 'value';
-                }else if('href' in targetEl){ //example 'a', 'link'
-                    valueProp = 'href';
-                }else{
-                    switch(valueType){
-                        case 'NumericRange':
-                            valueProp = 'ariaValueNow';
-                            break;
+    // mindRead(targetEl: Element, so: SetOptions){
+    //     let {valueProp, valueType, displayProp} = so;
+    //     const {localName} = targetEl;
+    //     if(valueProp === undefined){
+    //         if(valueType === 'Boolean'){
+    //             if('checked' in targetEl){
+    //                 valueProp = 'checked';
+    //             }else{
+    //                 valueProp = 'ariaChecked';
+    //             }
+    //         }else{
+    //             if('value' in targetEl && !'button-li'.includes(localName)){ //example 'input', 'output'
+    //                 valueProp = 'value';
+    //             }else if('href' in targetEl){ //example 'a', 'link'
+    //                 valueProp = 'href';
+    //             }else{
+    //                 switch(valueType){
+    //                     case 'NumericRange':
+    //                         valueProp = 'ariaValueNow';
+    //                         break;
 
-                    }
-                }
-            }
-            so.valueProp = valueProp;
+    //                 }
+    //             }
+    //         }
+    //         so.valueProp = valueProp;
 
-        }
-        if(displayProp === undefined){
-            switch(localName){
-                case 'input':
-                    //no value
-                    break;
-                default:
-                    switch(valueType){
-                        case 'NumericRange':
-                            displayProp = 'ariaValueText';
-                            break;
-                        default:
-                            displayProp = 'textContent';
-                            break;
-                    }
-            }
+    //     }
+    //     if(displayProp === undefined){
+    //         switch(localName){
+    //             case 'input':
+    //                 //no value
+    //                 break;
+    //             default:
+    //                 switch(valueType){
+    //                     case 'NumericRange':
+    //                         displayProp = 'ariaValueText';
+    //                         break;
+    //                     default:
+    //                         displayProp = 'textContent';
+    //                         break;
+    //                 }
+    //         }
 
-            so.displayProp = displayProp;
-        }
+    //         so.displayProp = displayProp;
+    //     }
         
 
-    }
+    // }
     #pureValue: TProp | undefined;
     async setValue(el: Element, val: TProp) {
         this.#pureValue = val;
