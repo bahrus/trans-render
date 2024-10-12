118910e38fa268d36f8dccf81508861e81cd384a
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-12 06:13:23 -0400

    0.0.847

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,12 +3,12 @@ import { PlaywrightTestConfig, devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run serve',
-    url: 'http://localhost:3030/',
+    url: 'http://localhost:8000/',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
-    baseURL: 'http://localhost:3030/',
+    baseURL: 'http://localhost:8000/',
   },
   projects: [
     {
