b57741a82837dbd9e8285fcbd27bb21aa4c85c7d
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-28 06:47:02 -0400

    0.0.848

--- env/IndexedDBObject.js
+++ env/IndexedDBObject.js
@@ -10,38 +10,8 @@ export class IndexedDBObject extends BaseIndexedDB {
     }
     async #setItem(key, value) {
         return await this.idbAction('put', { key, value });
-        // return new Promise((resolve, reject) => {
-        //   const transaction = this.db.transaction([this.storeName], 'readwrite');
-        //   const store = transaction.objectStore(this.storeName);
-        //   const request = store.put({ key, value });
-        //   request.onsuccess = () => {
-        //     resolve();
-        //   };
-        //   request.onerror = event => {
-        //     reject(`Failed to set item: ${event.target.error}`);
-        //   };
-        // });
     }
     async getProperty(key) {
         return await this.idbAction('get', key);
-        // return new Promise((resolve, reject) => {
-        //   const transaction = this.db.transaction([this.storeName], 'readonly');
-        //   const store = transaction.objectStore(this.storeName);
-        //   const request = store.get(key);
-        //   request.onsuccess = event => {
-        //     resolve(event.target.result ? event.target.result.value : null);
-        //   };
-        //   request.onerror = event => {
-        //     reject(`Failed to get item: ${event.target.error}`);
-        //   };
-        // });
     }
 }
-// // Usage example
-// (async () => {
-//   const db = new IndexedDBKeyValues('myDatabase', 'myStore');
-//   await db.init();
-//   await db.setItem('myKey', 'myValue');
-//   const value = await db.getItem('myKey');
-//   console.log('Stored value:', value);  // Output: Stored value: myValue
-// })();
--- env/IndexedDBObject.ts
+++ env/IndexedDBObject.ts
@@ -13,45 +13,13 @@ export class IndexedDBObject<TObject> extends BaseIndexedDB {
   
     async #setItem(key: keyof TObject, value: any) {
         return await this.idbAction('put', {key, value});
-      // return new Promise((resolve, reject) => {
-      //   const transaction = this.db.transaction([this.storeName], 'readwrite');
-      //   const store = transaction.objectStore(this.storeName);
-      //   const request = store.put({ key, value });
-  
-      //   request.onsuccess = () => {
-      //     resolve();
-      //   };
-  
-      //   request.onerror = event => {
-      //     reject(`Failed to set item: ${event.target.error}`);
-      //   };
-      // });
     }
   
     async getProperty(key: keyof TObject) {
         return await this.idbAction('get', key);
-      // return new Promise((resolve, reject) => {
-      //   const transaction = this.db.transaction([this.storeName], 'readonly');
-      //   const store = transaction.objectStore(this.storeName);
-      //   const request = store.get(key);
-  
-      //   request.onsuccess = event => {
-      //     resolve(event.target.result ? event.target.result.value : null);
-      //   };
-  
-      //   request.onerror = event => {
-      //     reject(`Failed to get item: ${event.target.error}`);
-      //   };
-      // });
+
     }
   }
   
-  // // Usage example
-  // (async () => {
-  //   const db = new IndexedDBKeyValues('myDatabase', 'myStore');
-  //   await db.init();
-  //   await db.setItem('myKey', 'myValue');
-  //   const value = await db.getItem('myKey');
-  //   console.log('Stored value:', value);  // Output: Stored value: myValue
-  // })();
+
   
