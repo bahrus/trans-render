84d43e0600e76afa49a60389281bf2557b2737cc
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-29 06:21:47 -0400

    0.0.848

---
+++ tests/env/IndexedDBObject.spec.mjs
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('IndexedDBTable', async ({ page }) => {
+  await page.goto('./tests/env/IndexedDBObject.html');
+  // wait for 1 second
+  await page.waitForTimeout(1000);
+  const editor = page.locator('#target');
+  await expect(editor).toHaveAttribute('mark', 'good');
+});
