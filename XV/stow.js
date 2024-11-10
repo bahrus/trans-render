f0c5a97f0677cc9a1b13f97a73383e6880731c75
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-11-10 13:36:30 -0500

    0.0.849

--- XV/stow.js
+++ XV/stow.js
@@ -1,6 +1,10 @@
 import { set } from './set.js';
 export async function stow(values, mapping) {
+    const ctx = {
+        USLs: []
+    };
     for (const key in mapping) {
-        await set(mapping[key], values[key]);
+        await set(mapping[key], values[key], ctx);
     }
+    window.postMessage([ctx.USLs]);
 }
--- XV/stow.ts
+++ XV/stow.ts
@@ -1,8 +1,12 @@
-import { USLMapping } from '../ts-refs/trans-render/XV/types'
+import { SavingContext, USLMapping } from '../ts-refs/trans-render/XV/types'
 import { set } from './set.js';
 
 export async  function stow(values: any, mapping: USLMapping){
+    const ctx: SavingContext = {
+        USLs: []
+    };
     for(const key in mapping){
-        await set(mapping[key], values[key]);
+        await set(mapping[key], values[key], ctx);
     }
+    window.postMessage([ctx.USLs]);
 }
