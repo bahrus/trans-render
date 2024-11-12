7da523f0d6a281c61626c7f058eabf15aa1c4b18
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-11-12 07:10:14 -0500

    0.0.850

---
+++ y.ts
@@ -0,0 +1,30 @@
+import { USLMapping } from './ts-refs/trans-render/XV/types';
+export async function y(schema: USLMapping){
+    return new Yield(schema);
+}
+
+export class Yield {
+    constructor(public schema: USLMapping){}
+    #to: string;
+    async to(guid: string){
+        this.#to = guid;
+    }
+
+    async yield() : Promise<any>{
+        return new Promise(async (resolve, reject) => {
+            const {draw} = await import('./XV/draw.js');
+            const drawn = await draw(this.schema);
+            if(this.#isComplete(drawn)){
+                resolve(drawn);
+                return;
+            }
+        });
+    }
+
+    #isComplete(obj: any){
+        for(const key in this.schema){
+            if(obj[key] === undefined || obj[key] === null) return false;
+        }
+        return true;
+    }
+}
