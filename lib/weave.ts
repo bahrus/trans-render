b303c1b17d267d885bcc77e8cdae304b7ced3c4c
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-11-16 13:36:06 -0500

    0.0.855

--- lib/weave.ts
+++ lib/weave.ts
@@ -46,6 +46,7 @@ export class Weave {
 export async function when(guid: string){
     if(values.has(guid)) return values.get(guid);
     const {waitForEvent} = await import('./waitForEvent.js');
+    if(values.has(guid)) return values.get(guid);
     await waitForEvent(window, guid);
     return values.get(guid);
 }
