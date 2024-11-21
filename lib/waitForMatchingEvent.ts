239c678fd683d1468a3318d5bf125f2f2ed5105f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-11-21 17:17:07 -0500

    0.0.857

--- lib/waitForMatchingEvent.ts
+++ lib/waitForMatchingEvent.ts
@@ -1,11 +1,11 @@
 export function waitForMatchingEvent<TEvent extends Event = Event>(
-    et: EventTarget, eventName: string, test: (evt: TEvent) => boolean,
+    et: EventTarget, eventName: string, test: (evt: TEvent) => Promise<boolean>,
     timeout: number = -1
 ): Promise<TEvent>{
     return new Promise((resolved, rejected) => {
         const ac = new AbortController();
         et.addEventListener(eventName, async e => {
-            if(!test(e as TEvent)) return;
+            if(!(await test(e as TEvent))) return;
             ac.abort();
             resolved(e as TEvent);
         }, {signal: ac.signal});
