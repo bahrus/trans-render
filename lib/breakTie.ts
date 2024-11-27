a6defcd3f22c76010cac4dfd5c6637dadc9a9471
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-11-27 07:35:09 -0500

    0.0.861

---
+++ lib/breakTie.ts
@@ -0,0 +1,36 @@
+const typeRankings = [
+    'undefined',
+    'null',
+    'string',
+    'boolean',
+    'number',
+    'bigint',
+    'symbol',
+    'object',
+    'function'
+];
+export function breakTie(lhs: any, rhs: any) {
+    if (lhs === rhs)
+        return 'eq';
+    const lhsType = lhs === null ? 'null' : typeof lhs;
+    const rhsType = rhs === null ? 'null' : typeof rhs;
+    const lhsTypeScore = typeRankings.indexOf(lhsType);
+    const rhsTypeScore = typeRankings.indexOf(rhsType);
+    if (lhsTypeScore > rhsTypeScore)
+        return 'lhs';
+    if (rhsTypeScore > lhsTypeScore)
+        return 'rhs';
+    switch (lhsType) {
+        case 'string':
+            if (lhs.length > rhs.length)
+                return 'lhs';
+            if (rhs.length > lhs.length)
+                return 'rhs';
+        default:
+            if (lhs > rhs)
+                return 'lhs';
+            if (rhs > lhs)
+                return 'rhs';
+    }
+    return 'eq';
+}
