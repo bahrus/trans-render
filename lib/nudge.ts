ce28494ceb6ad73f67866e42dbe2372af795f37d
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-04 19:53:03 -0500

    0.0.863

--- lib/nudge.ts
+++ lib/nudge.ts
@@ -1,16 +1,21 @@
+import {arr} from './arr.js';
 /**
  * Decrement "disabled" counter, remove when reaches 0
  * @param el 
  */
- export function nudge(el: Element, attr = 'disabled') { //TODO:  Share with be-observant
-    const da = el.getAttribute(attr);
-    if (da !== null) {
-        if (da.length === 0 || da === "1") {
-            el.removeAttribute(attr);
-            if(attr === 'disabled') (<any>el).disabled = false;
-        }
-        else {
-            el.setAttribute(attr, (parseInt(da) - 1).toString());
+ export function nudge(el: Element, attr: string | Array<string> = 'disabled') { //TODO:  Share with be-observant
+    const attrs = arr(attr);
+    for(const attr of attrs){
+        const da = el.getAttribute(attr);
+        if (da !== null) {
+            if (da.length === 0 || da === "1") {
+                el.removeAttribute(attr);
+                if(attr === 'disabled') (<any>el).disabled = false;
+            }
+            else {
+                el.setAttribute(attr, (parseInt(da) - 1).toString());
+            }
         }
     }
+
 }
