4d4b3411d9b7b7a7473cc8d9da91bc0335c0cfe7
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-26 08:35:29 -0500

    0.0.874

--- asmr/absorbFrom/UEMR.js
+++ asmr/absorbFrom/UEMR.js
@@ -7,65 +7,63 @@ export function UEMR(el, ao) {
     ao.isUE = true;
     let { evt, propToAbsorb } = ao;
     const { localName } = el;
-    if (propToAbsorb === undefined) {
-        switch (localName) {
-            case 'input':
-                if (evt === undefined)
-                    ao.evt = 'input';
-                if (propToAbsorb === undefined) {
-                    const { type } = el;
-                    switch (type) {
-                        case 'number':
-                        case 'range':
-                            ao.propToAbsorb = 'valueAsNumber';
-                            break;
-                        case 'checkbox':
-                            ao.propToAbsorb = 'checked';
-                            break;
-                        case 'date':
-                        case 'datetime-local':
-                            ao.propToAbsorb = 'valueAsDate';
-                            break;
-                        case 'file':
-                            throw 'NI';
-                        case 'image':
-                            throw 'NI';
-                        case 'color':
-                            throw 'NI';
-                        case 'radio':
-                            throw 'NI';
-                        case 'reset':
-                            throw 'NI';
-                        case 'time':
-                            throw 'NI';
-                        case 'week':
-                            throw 'NI';
-                        default:
-                            ao.propToAbsorb = 'value';
-                    }
+    switch (localName) {
+        case 'input':
+            if (evt === undefined)
+                ao.evt = 'input';
+            if (propToAbsorb === undefined) {
+                const { type } = el;
+                switch (type) {
+                    case 'number':
+                    case 'range':
+                        ao.propToAbsorb = 'valueAsNumber';
+                        break;
+                    case 'checkbox':
+                        ao.propToAbsorb = 'checked';
+                        break;
+                    case 'date':
+                    case 'datetime-local':
+                        ao.propToAbsorb = 'valueAsDate';
+                        break;
+                    case 'file':
+                        throw 'NI';
+                    case 'image':
+                        throw 'NI';
+                    case 'color':
+                        throw 'NI';
+                    case 'radio':
+                        throw 'NI';
+                    case 'reset':
+                        throw 'NI';
+                    case 'time':
+                        throw 'NI';
+                    case 'week':
+                        throw 'NI';
+                    default:
+                        ao.propToAbsorb = 'value';
                 }
-                break;
-            case 'button':
-                if (evt === undefined)
-                    ao.evt = 'click';
-                if (propToAbsorb === undefined) {
-                    ao.propToAbsorb = 'value';
-                }
-                break;
-            case 'form':
-                if (evt === undefined)
-                    ao.evt = 'input';
-                if (propToAbsorb === undefined) {
-                    ao.propToAbsorb = 'formData';
-                }
-                break;
-            default:
-                //content editable
-                if (evt === undefined)
-                    ao.evt = 'input';
-                if (propToAbsorb === undefined)
-                    ao.propToAbsorb = 'textContent';
-                break;
-        }
+            }
+            break;
+        case 'button':
+            if (evt === undefined)
+                ao.evt = 'click';
+            if (propToAbsorb === undefined) {
+                ao.propToAbsorb = 'value';
+            }
+            break;
+        case 'form':
+            if (evt === undefined)
+                ao.evt = 'input';
+            if (propToAbsorb === undefined) {
+                ao.propToAbsorb = 'formData';
+            }
+            break;
+        default:
+            //content editable
+            if (evt === undefined)
+                ao.evt = 'input';
+            if (propToAbsorb === undefined)
+                ao.propToAbsorb = 'textContent';
+            break;
     }
 }
