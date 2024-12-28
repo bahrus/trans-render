c0548f1dc3f20f7349d0053bbf64bc9184a6c751
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-28 14:05:46 -0500

    0.0.876

---
+++ lib/hookUp.ts
@@ -0,0 +1,44 @@
+import {HookupConfig} from '../ts-refs/trans-render/froop/types';
+import {Mount} from '../Mount.js';
+
+const cache = new Map<string, boolean>();
+
+export function hookUp<T extends HTMLElement>(jsExpr: string){
+    if(cache.has(jsExpr)) return;
+    const guid = `a_${crypto.randomUUID()}`;
+    const JSExpr = `
+    document.currentScript['${guid}'] = e => {
+        with(e.target){
+            ${jsExpr}
+        }
+    }
+    `;
+    const script = document.createElement('script');
+    script.innerHTML = JSExpr;
+    document.head.appendChild(script);
+    const commands = (<any>script)[guid] as HookupConfig;
+    cache.set(jsExpr, true);
+    const mnt = class extends Mount implements EventListenerObject {
+        #commandToMethodLookup = new Map<string, string>();
+        async connectedCallback(){
+            await super.connectedCallback();
+            for(const key in commands){
+                let handler = commands[key];
+                let commandType = key;
+                if(Array.isArray(handler)){
+                    commandType = handler[0];
+                    handler = handler[1];
+                }
+                this.addEventListener(commandType, this);
+            }
+        }
+
+        handleEvent(e: Event): void {
+            const handler = this.#commandToMethodLookup.get(e.type);
+            if(handler === undefined) throw 404;
+            (<any>this)[handler](this, e);
+        }
+    }
+    
+    
+}
