54e0583d5370c306a1f22aa7acb7531db4b3e94f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-01-19 08:36:24 -0500

    0.0.880

---
+++ demo/dtr-counter2.js
@@ -0,0 +1,68 @@
+import { Mount } from '../Mount.js';
+import { localize } from '../funions/Localizer.js';
+export class DTRCounter extends Mount {
+    localize = localize;
+    static config = {
+        shadowRootInit: {
+            mode: 'open'
+        },
+        mainTemplate: String.raw `<button part=down data-d=-1>-</button><data part=count aria-live=polite></data><button part=up data-d=1>+</button>`,
+        //mainTemplate: String.raw `<button part=down data-d=-1>-</button><span part=count aria-live=polite></span><button part=up data-d=1>+</button>`,
+        propDefaults: {
+            count: 30,
+        },
+        propInfo: {
+            ...super.mntCfgMxn.propInfo,
+            count: {
+                type: 'Number',
+                def: 30,
+                parse: true,
+                attrName: 'count',
+                propName: 'count',
+            }
+        },
+        actions: {
+            ...super.mntCfgMxn.actions
+        },
+        xform: {
+            '% count': 'localize',
+            button: {
+                m: {
+                    on: 'click',
+                    inc: 'count',
+                    byAmt: '?.dataset?.d',
+                },
+            }
+        },
+        styles: [
+            String.raw `
+<style>
+    :host{
+        display: block;
+    }
+    * {
+      font-size: 200%;
+    }
+
+    span {
+      width: 4rem;
+      display: inline-block;
+      text-align: center;
+    }
+
+    button {
+      width: 4rem;
+      height: 4rem;
+      border: none;
+      border-radius: 10px;
+      background-color: seagreen;
+      color: white;
+    }
+
+</style>
+            `
+        ]
+    };
+}
+await DTRCounter.bootUp();
+customElements.define('dtr-counter', DTRCounter);
---
+++ demo/dtr-counter2.ts
@@ -0,0 +1,79 @@
+import {MntCfg, Mount, MountActions, MountProps} from '../Mount.js';
+import {localize} from '../funions/Localizer.js';
+import { ITransformer, UnitOfWork } from '../ts-refs/trans-render/types.js';
+
+export interface DTRCounterProps {
+    count: number;
+} 
+
+export interface DTRCounterMethods {
+    localize(model: any, transformer: ITransformer<any, any>, uow: UnitOfWork<any, any>, matchingElement: Element): string | Partial<HTMLDataElement> | Partial<HTMLTimeElement> | undefined; 
+}
+export class DTRCounter extends Mount{
+    localize = localize;
+    static override config: MntCfg<DTRCounterProps & MountProps, DTRCounterMethods & MountActions> = {
+        shadowRootInit:{
+            mode: 'open'
+        },
+        mainTemplate: String.raw `<button part=down data-d=-1>-</button><data part=count aria-live=polite></data><button part=up data-d=1>+</button>`,
+        //mainTemplate: String.raw `<button part=down data-d=-1>-</button><span part=count aria-live=polite></span><button part=up data-d=1>+</button>`,
+        propDefaults:{
+            count: 30,
+        },
+        propInfo: {
+            ...super.mntCfgMxn.propInfo,
+            count: {
+                type: 'Number',
+                def: 30,
+                parse: true,
+                attrName: 'count',
+                propName: 'count',
+            }
+        },
+        actions:{
+            ...super.mntCfgMxn.actions
+        },
+        xform: {
+            '% count': 'localize',
+            button: {
+                m: {
+                    on: 'click',
+                    inc: 'count',
+                    byAmt: '?.dataset?.d',
+                },
+            }
+        },
+        styles: [
+            String.raw `
+<style>
+    :host{
+        display: block;
+    }
+    * {
+      font-size: 200%;
+    }
+
+    span {
+      width: 4rem;
+      display: inline-block;
+      text-align: center;
+    }
+
+    button {
+      width: 4rem;
+      height: 4rem;
+      border: none;
+      border-radius: 10px;
+      background-color: seagreen;
+      color: white;
+    }
+
+</style>
+            `
+        ]
+    } 
+}
+
+await DTRCounter.bootUp();
+
+customElements.define('dtr-counter', DTRCounter);
