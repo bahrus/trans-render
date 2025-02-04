34d01dde81bf1b2fc5fdb4872f1a6a1a0fb71625
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-02-04 11:30:49 -0500

    0.0.880

--- froop/RRMixin.js
+++ froop/RRMixin.js
@@ -1,1 +1,43 @@
-"use strict";
+export const publicPrivateStore = Symbol();
+//define a mixin class called RRMixin that implements RoundaboutReady
+export function RRMixin(Base) {
+    class RR extends Base {
+        propagator = new EventTarget();
+        [publicPrivateStore] = {};
+        #disconnectedAbortController;
+        get disconnectedSignal() {
+            return this.#disconnectedAbortController.signal;
+        }
+        constructor(...rest) {
+            super();
+            this.#disconnectedAbortController = new AbortController();
+        }
+        sleep;
+        awake() {
+            return new Promise((resolve, reject) => {
+                if (!this.sleep) {
+                    resolve();
+                    return;
+                }
+                const ac = new AbortController();
+                //I'm thinking this one isn't worth wrapping in an EventHandler, as the "closure"
+                //isn't accessing anything other than the resolve and abort controller, doesn't seem worth it.
+                this.propagator.addEventListener('sleep', e => {
+                    if (!this.sleep) {
+                        ac.abort();
+                        resolve();
+                    }
+                }, { signal: ac.signal });
+            });
+        }
+        nudge() {
+            const { sleep } = this;
+            this.sleep = sleep ? sleep - 1 : 0;
+        }
+        rock() {
+            const { sleep } = this;
+            this.sleep = sleep === undefined ? 1 : sleep + 1;
+        }
+    }
+    return RR;
+}
---
+++ froop/RRMixin.ts
@@ -0,0 +1,52 @@
+import {PropLookup, RoundaboutReady} from '../ts-refs/trans-render/froop/types';
+
+type Constructor<T = {}> = new (...args: any[]) => T;
+export const publicPrivateStore = Symbol();
+//define a mixin class called RRMixin that implements RoundaboutReady
+export function RRMixin<T extends Constructor, TProps=any>(Base: T) {
+  abstract class RR extends Base implements RoundaboutReady {
+    propagator = new EventTarget();
+    [publicPrivateStore]: Partial<TProps> = {}; 
+
+    #disconnectedAbortController: AbortController;
+
+    get disconnectedSignal(){
+        return this.#disconnectedAbortController.signal;
+    }
+
+    constructor(...rest: any[]){
+        super();
+        this.#disconnectedAbortController = new AbortController();
+    }
+
+    abstract covertAssignment(obj: TProps): Promise<void>;
+
+    sleep?: number | undefined;
+    awake() : Promise<void> {
+        return new Promise((resolve, reject) => {
+            if(!this.sleep) {
+                resolve();
+                return;
+            }
+            const ac = new AbortController();
+            //I'm thinking this one isn't worth wrapping in an EventHandler, as the "closure"
+            //isn't accessing anything other than the resolve and abort controller, doesn't seem worth it.
+            this.propagator.addEventListener('sleep', e => {
+                if(!this.sleep){
+                    ac.abort();
+                    resolve();
+                }
+            }, {signal: ac.signal});
+        })
+    }
+    nudge() {
+        const {sleep} = this;
+        this.sleep = sleep ? sleep - 1 : 0;
+    }
+    rock(){
+        const {sleep} = this;
+        this.sleep = sleep=== undefined ? 1 : sleep + 1;
+    }
+  }
+  return RR;
+}
