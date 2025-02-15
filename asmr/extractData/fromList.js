7cb013cd91b6f857480ac73c0a006215d4cf083f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-02-15 08:39:17 -0500

    0.0.884

--- asmr/extractData/fromList.js
+++ asmr/extractData/fromList.js
@@ -4,10 +4,6 @@ export function fromList(el, itemprops) {
     const itemScopes = el.querySelectorAll('[itemscope]');
     for (const itemScope of itemScopes) {
         const item = {};
-        // const item = {
-        //     key: itemScope.querySelector('[itemprop="key"]')?.textContent?.trim() ?? '',
-        //     value: Number( /** @type {HTMLDataElement} **/(itemScope.querySelector('data[itemprop="value"]'))?.value),
-        // };
         for (const prop of itemprops) {
             const itemProp = itemScope.querySelector(`[itemprop="${prop}"]`);
             if (itemProp === null) {
--- asmr/extractData/fromList.ts
+++ asmr/extractData/fromList.ts
@@ -7,10 +7,6 @@ export function fromList<TProps = any>(el: Element, itemprops: Array<keyof TProp
     
     for(const itemScope of itemScopes){
         const item : Partial<TProps> = {};
-        // const item = {
-        //     key: itemScope.querySelector('[itemprop="key"]')?.textContent?.trim() ?? '',
-        //     value: Number( /** @type {HTMLDataElement} **/(itemScope.querySelector('data[itemprop="value"]'))?.value),
-        // };
         for(const prop of itemprops){
             const itemProp = itemScope.querySelector(`[itemprop="${prop}"]`);
             if(itemProp === null){
