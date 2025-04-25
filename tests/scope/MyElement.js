9218f88e26955919e574192bd649ee5f74ce2b11
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-04-25 17:56:56 -0400

    0.0.893

---
+++ tests/scope/MyElement.js
@@ -0,0 +1,17 @@
+import { Scope } from '../../froop/Scope.js';
+export class MyElement extends Scope {
+    static config = {
+        propInfo: {
+            ishList: {},
+        },
+        compacts: {
+            when_ishList_changes_call_disp: 0,
+        }
+    };
+    disp(self) {
+        this.dispatchEvent(new Event('ishListChanged'));
+    }
+}
+MyElement.bootUp();
+customElements.define('my-element', MyElement);
+;
---
+++ tests/scope/MyElement.ts
@@ -0,0 +1,26 @@
+import {Scope} from '../../froop/Scope.js';
+import { IshConfig } from '../../ts-refs/trans-render/froop/types.js';
+
+interface Props {
+    ishList: Array<any>
+}
+
+interface Actions {
+    disp(self: Props): void;
+}
+export class MyElement extends Scope<Props> implements Actions {
+    static config : IshConfig<Props, Actions> = {
+        propInfo: {
+            ishList: {},
+        },
+        compacts: {
+            when_ishList_changes_call_disp: 0,
+        }
+    }
+    disp(self: Props): void {
+        this.dispatchEvent(new Event('ishListChanged'));
+    }
+}
+
+MyElement.bootUp();
+customElements.define('my-element', MyElement); ;
