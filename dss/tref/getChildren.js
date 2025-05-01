3353430c4b9eca5ebdfae02a103d8b9f1ca51531
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-05-01 06:05:41 -0400

    0.0.898

--- dss/tref/getChildren.js
+++ dss/tref/getChildren.js
@@ -1,7 +1,17 @@
 import { splitRefs } from 'mount-observer/itemRefUtils/splitRefs.js';
-export function getChildren(templ, refs) {
-    const rn = templ.getRootNode();
-    return splitRefs(refs)
+export function getChildren(el, refs, baseID) {
+    const rn = el.getRootNode();
+    if (refs === undefined || refs === null) {
+        refs = el.getAttribute('itemref');
+        if (refs === null)
+            return [];
+    }
+    let splitRef = splitRefs(refs);
+    if (baseID !== undefined) {
+        const prefix = baseID + '-';
+        splitRef = splitRef.filter(x => x.startsWith(prefix));
+    }
+    return splitRef
         .map(key => rn.getElementById(key))
         .filter(x => x !== null);
 }
--- dss/tref/getChildren.ts
+++ dss/tref/getChildren.ts
@@ -1,7 +1,16 @@
 import {splitRefs} from 'mount-observer/itemRefUtils/splitRefs.js';
-export function getChildren(templ: HTMLTemplateElement, refs: string){
-    const rn = templ.getRootNode() as DocumentFragment;
-    return splitRefs(refs)
+export function getChildren(el: Element, refs?: string | null, baseID?: string){
+    const rn = el.getRootNode() as DocumentFragment;
+    if(refs === undefined || refs === null){
+        refs = el.getAttribute('itemref');
+        if(refs === null) return [];
+    }
+    let splitRef = splitRefs(refs);
+    if(baseID !== undefined){
+        const prefix = baseID + '-';
+        splitRef = splitRef.filter(x => x.startsWith(prefix));
+    }
+    return splitRef
         .map(key => rn.getElementById(key))
         .filter(x => x !== null)
     ;
