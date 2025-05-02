8e53a3893046d0543de2331da9a0254cae9fdc45
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-05-02 07:24:59 -0400

    0.0.899

--- dss/tref/tail.js
+++ dss/tref/tail.js
@@ -1,3 +1,19 @@
+import { splitRefs } from 'mount-observer/refid/splitRefs.js';
 export function tail(el) {
-    const ;
+    const itemref = el.getAttribute('itemref');
+    if (itemref) {
+        const refs = splitRefs(itemref);
+        let ns = el.nextElementSibling;
+        if (ns === null)
+            return el;
+        let lastNS = ns;
+        while (ns) {
+            if (!refs.includes(ns.id)) {
+                return lastNS;
+            }
+            lastNS = ns;
+            ns = ns.nextElementSibling;
+        }
+        return lastNS;
+    }
 }
--- dss/tref/tail.ts
+++ dss/tref/tail.ts
@@ -1,3 +1,18 @@
+import {splitRefs} from 'mount-observer/refid/splitRefs.js';
 export function tail(el: Element){
-    const 
+    const itemref = el.getAttribute('itemref');
+    if(itemref){
+        const refs = splitRefs(itemref);
+        let ns = el.nextElementSibling;
+        if(ns === null) return el;
+        let lastNS = ns;
+        while(ns){
+            if(!refs.includes(ns.id)){
+                return lastNS;
+            }
+            lastNS = ns;
+            ns = ns.nextElementSibling;
+        }
+        return lastNS;
+    }
 }
