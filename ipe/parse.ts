134d7a3e16bc3a06a8b0afef49d20383e7d78427
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-09-01 12:14:04 -0400

    0.0.930

---
+++ ipe/parse.ts
@@ -0,0 +1,11 @@
+import { IPE } from "../ts-refs/trans-render/dss/types";
+import { splitOnce } from "../lib/splitOnce.js";
+export function parse(s: string) : IPE {
+    const [nonEvtPart, evtName] = splitOnce(s, '::');
+    const [id, path] = splitOnce(nonEvtPart, '.?');
+    return {
+        id,
+        path: path === undefined ? undefined : `.?${path}`,
+        evtName
+    };
+}
