ca585bee84125e65173cac747747598c16525ed7
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-09-01 21:20:02 -0400

    0.0.935

--- ipe/parse.js
+++ ipe/parse.js
@@ -3,11 +3,14 @@ export function parse(s) {
     const [nonAsPart, asOrUndefined] = splitOnce(s, ' as ');
     const [nonEvtPart, evtName] = splitOnce(nonAsPart, '::');
     const [id, path] = splitOnce(nonEvtPart, '.?');
-    let revisedID = id === '' ? undefined : id.substring(1);
+    let revisedID = id;
     let constVal = undefined;
     if (revisedID?.startsWith('`') && revisedID.endsWith('`')) {
         constVal = revisedID.substring(1, revisedID.length - 1);
         revisedID = undefined;
+    }
+    else {
+        revisedID = id === '' ? undefined : id.substring(1);
     }
     return {
         id: revisedID,
--- ipe/parse.ts
+++ ipe/parse.ts
@@ -4,12 +4,16 @@ export function parse(s: string) : IPE {
     const [nonAsPart, asOrUndefined] = splitOnce(s, ' as ');
     const [nonEvtPart, evtName] = splitOnce(nonAsPart, '::');
     const [id, path] = splitOnce(nonEvtPart, '.?');
-    let revisedID =  id === '' ? undefined : id.substring(1);
+    let revisedID = id as string | undefined;
     let constVal = undefined;
     if(revisedID?.startsWith('`') && revisedID.endsWith('`')){
         constVal = revisedID.substring(1, revisedID.length - 1);
         revisedID = undefined;
+    }else{
+        revisedID =  id === '' ? undefined : id.substring(1);
     }
+    
+    
     return {
         id: revisedID,
         path: path === undefined ? undefined : `.?${path}`,
