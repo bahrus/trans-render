d136d09904069778a0c91ec2c2a2e08729e68659
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-09-06 11:08:46 -0400

    0.0.941

---
+++ dss/find.js
@@ -0,0 +1,9 @@
+async function find(el, specifier) {
+    const { id, host } = specifier;
+    if (id !== undefined)
+        return el.getRootNode().getElementById(id);
+    if (host === true)
+        return el.getRootNode().host;
+    return (await el.getHostish());
+}
+export {};
---
+++ dss/find.ts
@@ -0,0 +1,8 @@
+import { Specifier } from "../ts-refs/trans-render/dss/types";
+
+async function find(el: Element, specifier: Specifier){
+    const {id, host} = specifier;
+    if(id !== undefined) return (el.getRootNode() as DocumentFragment).getElementById(id);
+    if(host === true) return (<any>el.getRootNode()).host as HTMLElement | undefined;
+    return (await (<any>el).getHostish()) as EventTarget;
+}
