046eb49d19b4d3849319d7abb2970c5c2bb066b1
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-09-09 20:38:21 -0400

    0.0.950

--- dss/find.js
+++ dss/find.js
@@ -1,8 +1,12 @@
 import 'mount-observer/refid/hostish.js';
+function getByIdInclusive(fragment, id) {
+    if (fragment instanceof Element && fragment.id === id)
+        return fragment;
+}
 export async function find(el, specifier) {
     const { id, host } = specifier;
     if (id !== undefined)
-        return el.getRootNode().getElementById(id);
+        return getByIdInclusive(el.getRootNode(), id);
     if (host === true)
         return el.getRootNode().host;
     return (await el.hostish());
--- dss/find.ts
+++ dss/find.ts
@@ -1,9 +1,13 @@
 import 'mount-observer/refid/hostish.js';
 import { Specifier } from "../ts-refs/trans-render/dss/types";
 
+function getByIdInclusive(fragment: Node, id: string){
+    if(fragment instanceof Element && fragment.id === id) return fragment;
+}
+
 export async function find(el: Element, specifier: Specifier){
     const {id, host} = specifier;
-    if(id !== undefined) return (el.getRootNode() as DocumentFragment).getElementById(id);
+    if(id !== undefined) return getByIdInclusive(el.getRootNode(), id);
     if(host === true) return (<any>el.getRootNode()).host as HTMLElement | undefined;
     return (await (<any>el).hostish()) as EventTarget;
 }
