7b5e938a4d2cbb24d9b0a5c55dcd1549aad93bc7
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-09-10 05:25:41 -0400

    0.0.951

--- tests/dss/AllTests.spec.mjs
+++ tests/dss/AllTests.spec.mjs
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test('eventCheck', async ({ page }) => {
-  await page.goto('./tests/scope/eventCheck.html');
+test('dss/AllTests', async ({ page }) => {
+  await page.goto('./tests/dss/AllTests.html');
   // wait for 1 second
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(500);
   const editor = page.locator('#target');
   await expect(editor).toHaveAttribute('mark', 'good');
 });
