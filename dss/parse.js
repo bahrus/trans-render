cdd341611c5cbf4470b249caafdd3591d5d2ee4d
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-09-10 05:15:28 -0400

    0.0.951

--- dss/parse.js
+++ dss/parse.js
@@ -1,21 +1,36 @@
 import { splitOnce } from "../lib/splitOnce.js";
 const host = ':host()';
 const oc = '?.';
+const mySelf = '$0';
 export function parse(s) {
     const [beforeEvtPart, evtNameOrUndefined] = splitOnce(s, '::');
     const [beforeAsPart, asOrUndefined] = splitOnce(beforeEvtPart, '-as-');
-    let revisedID, constVal, prop, path, enhKey, ish = false, targetHost = false, ext;
+    let revisedID, constVal, prop, path, enhKey, ish = false, targetHost = false, ext, isSelf = false;
+    const startWithHash = beforeAsPart.startsWith('#');
+    const startWithSelf = beforeAsPart.startsWith(mySelf);
     if (beforeAsPart.startsWith('`') && beforeAsPart.endsWith('`')) {
         constVal = beforeAsPart.substring(1, beforeAsPart.length - 1);
     }
-    else if (beforeAsPart.startsWith('#') || beforeAsPart.startsWith(host)) {
+    else if (startWithHash || beforeAsPart.startsWith(host) || startWithSelf) {
         const [beforePropPath, propPath] = splitOnce(beforeAsPart, oc);
         if (propPath !== undefined)
             [prop, path] = splitOnce(propPath, oc);
-        if (beforeAsPart.startsWith('#')) {
+        if (startWithHash) {
             revisedID = beforePropPath.substring(1);
             [revisedID, ext] = splitOnce(revisedID, '+'); //untested
             if (ext !== undefined) {
+                if (ext === 'ish') {
+                    ish = true;
+                }
+                else {
+                    enhKey = ext;
+                }
+            }
+        }
+        else if (startWithSelf) {
+            isSelf = true;
+            if (beforePropPath[2] === '+') {
+                ext = beforePropPath.substring(3);
                 if (ext === 'ish') {
                     ish = true;
                 }
@@ -41,6 +56,7 @@ export function parse(s) {
         constVal,
         ish,
         enhKey,
-        host: targetHost
+        host: targetHost,
+        self: isSelf,
     };
 }
--- dss/parse.ts
+++ dss/parse.ts
@@ -2,23 +2,35 @@ import { DSS, Specifier, asOptions } from "../ts-refs/trans-render/dss/types";
 import { splitOnce } from "../lib/splitOnce.js";
 const host = ':host()';
 const oc = '?.';
+const mySelf = '$0';
 export function parse(s: DSS) : Specifier {
     const [beforeEvtPart, evtNameOrUndefined] = splitOnce(s, '::');
     const [beforeAsPart, asOrUndefined] = splitOnce(beforeEvtPart, '-as-');
     let revisedID: string | undefined, constVal: string | undefined, prop: string | undefined, 
         path: string | undefined, enhKey: string | undefined, ish = false, 
-        targetHost = false, ext: string | undefined
+        targetHost = false, ext: string | undefined, isSelf = false
     ;
-
+    const startWithHash = beforeAsPart.startsWith('#');
+    const startWithSelf = beforeAsPart.startsWith(mySelf);
     if(beforeAsPart.startsWith('`') && beforeAsPart.endsWith('`')){
         constVal = beforeAsPart.substring(1, beforeAsPart.length - 1);
-    }else if(beforeAsPart.startsWith('#') || beforeAsPart.startsWith(host)){
+    }else if(startWithHash || beforeAsPart.startsWith(host) || startWithSelf){
         const [beforePropPath, propPath] = splitOnce(beforeAsPart, oc);
         if(propPath !== undefined) [prop, path] = splitOnce(propPath, oc);
-        if(beforeAsPart.startsWith('#')){
+        if(startWithHash){
             revisedID = beforePropPath.substring(1);
             [revisedID, ext] = splitOnce(revisedID, '+'); //untested
             if(ext !== undefined){
+                if(ext === 'ish'){
+                    ish = true;
+                }else{
+                    enhKey = ext;
+                }
+            }
+        }else if(startWithSelf){
+            isSelf = true;
+            if(beforePropPath[2] === '+'){
+                ext = beforePropPath.substring(3);
                 if(ext === 'ish'){
                     ish = true;
                 }else{
@@ -44,6 +56,7 @@ export function parse(s: DSS) : Specifier {
         constVal,
         ish,
         enhKey,
-        host: targetHost
+        host: targetHost,
+        self: isSelf,
     };
 }
